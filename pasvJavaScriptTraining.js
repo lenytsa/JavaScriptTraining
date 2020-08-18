6edf6f0fba02fbbd1069dca976bf46b647ade86e
yelenaportnaya <yportnaya@hotmail.com>
2020-08-18 13:50:46 -0700

    Сколько цифр содержит число: 1, 2 или 3? https://app.pasv.us/course/5d1cbf67086fc30038bf1859/lesson/5ddc43e2a9ccbf003caa26d3

---
+++ pasvJavaScriptTraining.js
@@ -0,0 +1,20 @@
+// //https://app.pasv.us/course/5d1cbf67086fc30038bf1859/lesson/5ddc43e2a9ccbf003caa26d3
+// Описание числа
+// Дано целое число т 1 до 999. Получите описание числа в виде строки: "one-digit number",
+// "two-digit number", "three-digit number".
+//
+//     Напишите функцию с именем numberDescription, которая принимает число num в качестве аргумента и возвращает
+//
+// "one-digit number", если число однозначное,
+//     "two-digit number", если число двухзначное,
+//     "three-digit number", если число трехзначное.
+//     Примеры:
+//
+// функция numberDescription(1) должна возвратить "one-digit number";
+// функция numberDescription(12) должна возвратить "two-digit number";
+// функция numberDescription(134) должна возвратить "three-digit number".
+
+function numberDescription(num){
+    return num.toString().length;
+}
+console.log(numberDescription(311));
