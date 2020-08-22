ae784071759f63c7fb25f13bd0c8d8522c4745ef
yelenaportnaya <yportnaya@hotmail.com>
2020-08-21 23:29:19 -0700

    CodeWars: 101 Dalmatians - squash the bugs, not the dogs! https://www.codewars.com/kata/56f6919a6b88de18ff000b36/solutions/javascript/all/best_practices

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -114,6 +114,7 @@ function describeAge(age) {
         return "You're a(n) elderly";
     }
 }
+console.log('-------------------------------------')
 
 function describeAge1(age) {
     //let s = 'You\'re a(n) ';
@@ -121,7 +122,39 @@ function describeAge1(age) {
     //return (age<13)?`${s}kid`:(age <18)?`${s}teenager`:(age<65)?`${s}adult`:`${s}elderly`;
     //return `You're a(n) ${age<13?'kid':age<18?'teenager':age<65?'adult':'elderly'}`
     return `You're a(n) ${age < 13 ? 'kid' : age < 18 ? 'teenager' : age < 65 ? 'adult' : 'elderly'}`;
-
 }
 console.log(describeAge1(68));
+console.log('-------------------------------------')
+
+//CodeWars: 101 Dalmatians - squash the bugs, not the dogs!
+//https://www.codewars.com/kata/56f6919a6b88de18ff000b36/train/javascript
+function howManyDalmatians(number) {
+
+    var dogs= ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+
+    var respond = (number <= 10) ? dogs[0]: (number <= 50) ? dogs[1] : (number <=100) ?  dogs[2] : dogs[3];
+    return respond
+}
+function howManyDalmatians2 (number) {
+    let dogs = [
+        'Hardly any',
+        'More than a handful!',
+        'Woah that\'s a lot of dogs!',
+        '101 DALMATIANS!!!'
+    ]
+    let respond = number <= 10 ? dogs[0]
+        : number <= 50 ? dogs[1]
+            : number == 101 ? dogs[3]
+                : dogs[2]
+    return respond
+}
+console.log(howManyDalmatians(26));
+console.log(howManyDalmatians(100));
+console.log(howManyDalmatians(101));
+console.log('-------------------------------------')
+console.log(howManyDalmatians2(26));
+console.log(howManyDalmatians2(100));
+console.log(howManyDalmatians2(101));
+console.log('-------------------------------------')
+
 
