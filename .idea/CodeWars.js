2239b366afe31be4053fd62eaaa30823634825dc
yelenaportnaya <yportnaya@hotmail.com>
2020-08-22 22:43:36 -0700

    CodeWars: Basic Mathematical Operations https://www.codewars.com/kata/57356c55867b9b7a60000bd7/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -251,9 +251,18 @@ function howManydays(month){
   return days;
 }
 console.log(howManydays(132));
-
-
 console.log('-------------------------------------')
+//Basic Mathematical Operations
+//https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/javascript
+function basicOp(operation, value1, value2)
+{
+   switch (operation) {
+       case '+': return value1 + value2;
+       case '-': return value1 - value2;
+       case '*': return value1 * value2;
+       case '/': return value1 / value2;
+   }
+}
 
 
 
