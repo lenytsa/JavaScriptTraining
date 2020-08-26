c6aac168f332fd82d995286ed7b49e80d73e204a
yelenaportnaya <yportnaya@hotmail.com>
2020-08-25 22:16:16 -0700

    Find prime number

---
+++ otherExamples.js
@@ -0,0 +1,19 @@
+//Лекция 6. Циклы, в которых вызываются функции
+
+//   Задача 1: - определите количество простых чисел в диапазоне от m до n
+//   Простое число - число, у которого нет делителей, кроме
+//   1 и самого числа. Например, 2, 3, 5, 7, 11 - простые числа
+//  In math, prime numbers are whole numbers greater than 1, that have only two factors – 1 and the number itself.
+// Prime numbers are divisible only by the number 1 or itself.
+
+function isPrime(n){
+    if(n< 2) return false;
+        for(let i=2; i<n; i++){
+    if(n%i === 0)
+        return false;
+    }
+return true;
+}
+console.log(isPrime(5)); //prime number
+console.log(isPrime(6)); //not prime number
+console.log("----------------------------------")
