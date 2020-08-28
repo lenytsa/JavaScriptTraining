998b6c55a2bb7e0176a29a8397059588b10ad0cf
yelenaportnaya <yportnaya@hotmail.com>
2020-08-27 23:35:26 -0700

    CodeWars: Find the divisors! https://www.codewars.com/kata/544aed4c4a30184e960010f4/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -437,4 +437,30 @@ function getDivisorsCnt(n){
     }
     return count;
 }
+console.log('-----------------------------------------')
+//Find the divisors!
+//https://www.codewars.com/kata/544aed4c4a30184e960010f4/solutions/javascript
+// Create a function named divisors/Divisors that takes an integer
+// n > 1 and returns an array with all of the integer's divisors(except for 1' +
+// ' and the number itself), from smallest to largest. If the number is prime return the string ' +
+// ''(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+// divisors(12); // should return [2,3,4,6]
+// divisors(25); // should return [5]
+// divisors(13); // should return "13 is prime"
+
+function divisors(n){
+    let arrDiv = [];
+    //let count = 0;
+    for(let i=0; i<=n; i++){
+        if(n%i === 0) {
+            //count++
+            if (i!==1 && i !==n) {
+                arrDiv.push(i);
+            }
+        }
+    }
+    return arrDiv.length === 0 ? `${n} is prime`: arrDiv;
+}
+console.log(divisors(15));
+console.log(divisors(13));
 
