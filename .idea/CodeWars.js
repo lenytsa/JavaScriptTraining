11ba06a6fef95e3b886e8e3ce71f9cbfee4f1771
yelenaportnaya <yportnaya@hotmail.com>
2020-08-28 00:36:49 -0700

    CodeWars: Sum of the first nth term of Series https://www.codewars.com/kata/555eded1ad94b00403000071/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -450,10 +450,8 @@ console.log('-----------------------------------------')
 
 function divisors(n){
     let arrDiv = [];
-    //let count = 0;
     for(let i=0; i<=n; i++){
         if(n%i === 0) {
-            //count++
             if (i!==1 && i !==n) {
                 arrDiv.push(i);
             }
@@ -463,4 +461,29 @@ function divisors(n){
 }
 console.log(divisors(15));
 console.log(divisors(13));
+console.log('-----------------------------------------')
 
+//Sum of the first nth term of Series
+//https://www.codewars.com/kata/555eded1ad94b00403000071/train/javascript
+// Task:
+//     Your task is to write a function which returns the sum of following series upto nth term(parameter).
+//     Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+//     You need to round the answer to 2 decimal places and return it as String.
+//     If the given value is 0 then it should return 0.00
+// You will only be given Natural Numbers as arguments.
+function SeriesSum(n)
+{
+    let sum = 0;
+    let d = 1;
+    for(let i=1; i<=n; i++)
+    {
+        sum = sum + 1/(d);
+        d += 3;
+    }
+    return sum.toFixed(2);
+}
+console.log(SeriesSum(2));//1.25
+console.log(SeriesSum(3));//1.39
+console.log(SeriesSum(4));//1.49
+console.log(SeriesSum(5));//1.57
