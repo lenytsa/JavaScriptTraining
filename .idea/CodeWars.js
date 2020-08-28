ef779f48a27d314d2389bfc13ca96f073881300f
yelenaportnaya <yportnaya@hotmail.com>
2020-08-28 12:15:10 -0700

    CodeWars: Beginner Series #3 Sum of Numbers https://www.codewars.com/kata/55f2b110f61eb01779000053/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -504,3 +504,39 @@ console.log(summation(2));
 console.log(summation(8));
 //// Short Solution
 // const summation = n => n * (n + 1) / 2
+
+console.log('-----------------------------------------')
+
+//Beginner Series #3 Sum of Numbers
+// Given two integers a and b, which can be positive or negative,
+// find the sum of all the numbers between including them too and return it. If the two numbers are equal return a or b.
+//     Note: a and b are not ordered!
+// GetSum(1, 0) == 1   // 1 + 0 = 1
+// GetSum(1, 2) == 3   // 1 + 2 = 3
+// GetSum(0, 1) == 1   // 0 + 1 = 1
+// GetSum(1, 1) == 1   // 1 Since both are same
+// GetSum(-1, 0) == -1 // -1 + 0 = -1
+// GetSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
+
+function getSum( a, b ) {
+    let sum = 0;
+    let h,l;
+    if (a === b) return a;
+    if(a>b){
+        h=a;
+        l=b;
+    }
+    else if(b>a){
+        h=b;
+        l=a;
+    }
+    for(let i=l; i<=h; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+console.log(getSum(1, 2));
+console.log(getSum(2, 2));
+console.log(getSum(-1, 0));
+console.log(getSum(0, -1));//-1
