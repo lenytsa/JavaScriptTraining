a362346bb4880f2a90390e0b4bceccf5aba73125
yelenaportnaya <yportnaya@hotmail.com>
2020-09-01 00:47:24 -0700

    CodeWars: Training JS #11: loop statement --break,continue https://www.codewars.com/kata/5721c189cdd71194c1000b9b/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -683,4 +683,25 @@ function padIt(str,n){
 }
 console.log(padIt("a",4));
 console.log(padIt("a",5));
+console.log('++-----------------------------------------')
 
+//CodeWars: Training JS #11: loop statement --break,continue
+//https://www.codewars.com/kata/5721c189cdd71194c1000b9b/train/javascript
+function grabDoll(dolls){
+    var bag=[];
+   for(let i=0; i<dolls.length; i++){
+       if(dolls[i]==='Hello Kitty'||dolls[i]==='Barbie doll' ){
+           bag.push(dolls[i])
+       }
+       else {
+           continue;
+       }
+       if(bag.length===3){
+           break;
+       }
+   }
+    return bag;
+}
+console.log(grabDoll(["Mickey Mouse","Hello Kitty","Snow white"]));
+console.log(grabDoll(["Mickey Mouse","Hello Kitty","Hello Kitty","Snow white"]));
+console.log(grabDoll(["Mickey Mouse","Barbie doll","Hello Kitty","Hello Kitty","Hello Kitty","Snow white"]));
