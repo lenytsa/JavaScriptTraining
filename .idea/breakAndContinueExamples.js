213e8feaf249a3e8932d1ad4f47d402ba895f8e6
yelenaportnaya <yportnaya@hotmail.com>
2020-08-31 23:33:50 -0700

    Найдите в массиве первый четный элемент и его индекс.Using break; https://app.pasv.us/course/5d1cbf67086fc30038bf1859/lesson/5ddc36baa9ccbf003caa2679

--- .idea/breakAndContinueExamples.js
+++ .idea/breakAndContinueExamples.js
@@ -89,3 +89,27 @@ return false;
 }
 console.log(isNegativeInArray([1, 2, 3, 9, 0]));
 console.log(isNegativeInArray([1, 2, -3, 9, 0]));
+console.log('--------------------------------------')
+
+//Найдите в массиве первый четный элемент и его индекс.
+// Напишите функцию с именем firstEvenElement, которая принимает массив arr в качестве аргумента и
+// возвращает первый четный элемент и его индекс в виде массива [element, index]. Если в массиве нет ни
+// одного четного элемента,
+// вернуть пустой массив []. В решении необходимо использовать оператор break.
+//     функция firstEvenElement([1, 2, 3, 4, 9, 0]) должна возвратить [2, 1];
+// функция firstEvenElement([2, 1, -3, 4, 3]) должна возвратить [2, 0];
+// функция firstEvenElement([9, 1, -3, -4, 12, 6]) должна возвратить [-4, 3];
+// функция firstEvenElement([9, 1, -3, 3, 11]) должна возвратить [].
+function firstEvenElement(arr){
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]%2 ===0){
+            return [arr[i],i];
+            break;
+        }
+    }
+    return []
+}
+console.log(firstEvenElement([1, 2, 3, 4, 9, 0]));
+console.log(firstEvenElement([2, 1, -3, 4, 3]));
+console.log(firstEvenElement([9, 1, -3, 3, 11]));
+console.log(firstEvenElement([9, 1, -3, -4, 12, 6]));
