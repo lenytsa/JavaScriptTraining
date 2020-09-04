e20b80aca2ee78b39df789d3d5f622c3dd9b0d97
yelenaportnaya <yportnaya@hotmail.com>
2020-09-03 23:52:05 -0700

    CodeWars: ATM https://www.codewars.com/kata/5635e7cb49adc7b54500001c/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -798,4 +798,20 @@ function sum (numbers) {
     return s;
 
 };
+console.log('-------------------')
+//ATM
+//https://www.codewars.com/kata/5635e7cb49adc7b54500001c/train/javascript
+// There is enough money available on ATM in nominal value 10, 20, 50, 100, 200 and 500 dollars.
+//     You are given money in nominal value of n with 1<=n<=1500.
+//  Try to find minimal number of notes that must be used to repay in dollars, or output -1 if it is impossible.
+function solve(n) {
+   let count = 0;
+   if(n%10 !==0) return -1;
+   let arr = [500,200,100,50,20,10];
+   for(let elm of arr){
+       while(n >= elm){ n = n - elm; count++}
+   }
+   return count;
+}
+console.log(solve(770));
 
