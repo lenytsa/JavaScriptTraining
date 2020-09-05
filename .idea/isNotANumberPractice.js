5fc05b9c9357dbaf90f4f334dab3cb3c335fb615
yelenaportnaya <yportnaya@hotmail.com>
2020-09-04 21:54:57 -0700

    Является ли величина NaN? https://app.pasv.us/course/5d1cbf67086fc30038bf1859/lesson/5dcfad79d95082003c2659ce

--- .idea/isNotANumberPractice.js
+++ .idea/isNotANumberPractice.js
@@ -11,9 +11,38 @@
 // функция isValid(25) должна возвратить "Value is valid";
 // функция isValid(1/0) должна возвратить "Value is valid";
 // функция isValid('abc') должна возвратить "Value is valid".
-
+console.log('-----------------------')
 function isValid(value){
     return Number.isNaN(value) ? 'Value is not a number': 'Value is valid';
 }
 console.log(isValid(1/0));
 console.log(isValid('abc' * 3));
+console.log('-----------------------')
+
+//Является ли величина NaN?
+// Напишите функцию isArgNaN, которое проверяет, содержит ли аргумент value значение NaN.
+//
+//     Если аргумент является NaN, возвратить значение NaN.
+//
+//     В противном случае возвратить значение величины value.
+//
+//     Используйте в решении функцию isNaN(value)
+    // функция isArgNaN(NaN) должна возвратить NaN;
+    // функция isArgNaN('hello' * 2) должна возвратить NaN;
+    // функция isArgNaN(NaN + 1) должна возвратить NaN;
+    // функция isArgNaN(0/0) должна возвратить NaN;
+    // функция isArgNaN('abc') должна возвратить NaN;
+    // функция isArgNaN('a') должна возвратить NaN";
+    // функция isArgNaN('2,5') должна возвратить NaN";
+    // функция isArgNaN(25) должна возвратить 25;
+    // функция isArgNaN(1/0) должна возвратить Infinity;
+    // функция isArgNaN('23' * 3) должна возвратить 69;
+    // функция isArgNaN('2.343') должна возвратить "2.343";
+    // функция isArgNaN(true) должна возвратить true;
+function isArgNaN(value){
+    return isNaN(value) ? NaN:  value;
+}
+console.log(isArgNaN(NaN));
+console.log(isArgNaN('hello' * 2));
+console.log(isArgNaN(25));
+console.log(isArgNaN(true));
