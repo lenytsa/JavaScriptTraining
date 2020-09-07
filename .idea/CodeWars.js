6c16ebfa6169a93f88a484733524bf4c3ad89824
yelenaportnaya <yportnaya@hotmail.com>
2020-09-06 22:23:36 -0700

    CodeWars: Return Negative https://www.codewars.com/kata/55685cd7ad70877c23000102/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -869,3 +869,16 @@ console.log(Number.isSafeInteger(676876360));
 console.log(Number.isSafeInteger(-1308e+500));
 console.log(Number.isSafeInteger(Infinity));
 
+//Return Negative
+//https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+//     makeNegative(1); // return -1
+// makeNegative(-5); // return -5
+// makeNegative(0); // return 0
+// makeNegative(0.12); // return -0.12
+function makeNegative(num) {
+    if(num<0) return num;
+    if(num>0) return -num;
+    if(num===0) return 0;
+}
+
