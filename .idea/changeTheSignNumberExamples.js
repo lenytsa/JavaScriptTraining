8532245e33b0559206d2ee811c8cb377374c7efb
yelenaportnaya <yportnaya@hotmail.com>
2020-09-06 19:23:04 -0700

    Изменение знака числа большего из двух чисел https://app.pasv.us/course/5d1cbf67086fc30038bf1859/lesson/5dcfa9dcd95082003c265985

--- .idea/changeTheSignNumberExamples.js
+++ .idea/changeTheSignNumberExamples.js
@@ -9,3 +9,20 @@ function oppositeNumber(num){
 return num<0? (-num): -num;
 }
 console.log(oppositeNumber(7));
+console.log('------------------------------------')
+//Изменение знака числа большего из двух чисел
+// Напишите функцию с именем changeMax, которая принимает два числа a и b в качестве аргументов,
+//     меняет знак большего из чисел на противоположный и возвращает это число.
+//     Если числа равны, вернуть сообщение "The numbers are equal".
+// функция changeMax(9, 4) должна возвратить -9;
+// функция changeMax(1, 6) должна возвратить -6;
+// функция changeMax(0, 4) должна возвратить -4;
+// функция changeMax(-1, -3) должна возвратить 1;
+// функция changeMax(4, 4) должна возвратить "The numbers are equal".
+function changeMax(a, b){
+    if(a===b) return 'The numbers are equal';
+    if(a > b) return -a;
+    if(a < b) return -b;
+}
+console.log( changeMax(1, 6));
+console.log( changeMax(1, 1));
