ad4a6ebee3065992540fe774cef2e702a362d125
yelenaportnaya <yportnaya@hotmail.com>
2020-09-07 21:59:53 -0700

    BASIC: Making Six Toast. https://www.codewars.com/kata/5834fec22fb0ba7d080000e8/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -915,3 +915,21 @@ console.log(invert([1,2,3,4,5]));
 console.log(invert([-4,-5]));
 let num = -3; num = -num;
 console.log(num);
+
+//BASIC: Making Six Toast.
+// Story:
+//     You are going to make toast fast, you think that
+// you should make multiple pieces of toasts and once. So, you try to make 6 pieces of toast.
+//     You forgot to count the number of toast you put into there, you don't know if you put exactly
+//     six pieces of toast into the toasters.
+//
+// Define a function that counts how many more (or less) pieces of toast you need in the toasters. Even
+// though you need more or
+// less, the number will still be positive, not negative.
+// You must return the number of toast the you need to put in (or to take out). In case of 5 you can still put 1 toast in:
+// six_toast(5) == 1
+function sixToast(num) {
+    let ps =6;
+    return Math.abs(ps - num);
+}
+
