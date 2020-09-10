0865f7bdb9d7bd8c366d040fb5641cd226a77db0
yelenaportnaya <yportnaya@hotmail.com>
2020-09-10 00:38:50 -0700

    CodeWars:Square Every Digit https://www.codewars.com/kata/546e2562b03326a88e000020/train/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -960,3 +960,21 @@ console.log(elevator(0, 1, 1));
 console.log('-------------------------------------')
 let num1 = -(Math.abs(-10))
 console.log(num1)
+
+//Square Every Digit
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+//     For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+// Note: The function accepts an integer and returns an integer
+
+function squareDigits(num) {
+    let result=[];
+    let separatedDigits = num.toString().split('');
+    for(let elm of separatedDigits){
+        result.push(Math.pow(elm,2));
+    }
+    return +result.join(''); //return as number not as string
+}
+console.log(squareDigits(9119));
+console.log(squareDigits(123));
+
+
