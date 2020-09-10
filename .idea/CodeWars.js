4a79226ae2c60aef1e20e1b900848ad8397589bc
yelenaportnaya <yportnaya@hotmail.com>
2020-09-10 11:29:16 -0700

    CodeWars:N-th Power https://www.codewars.com/kata/57d814e4950d8489720008db/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -994,7 +994,27 @@ function squares(x, n) {
 }
 console.log(squares(2,5));
 console.log(squares(3,3));
-
+//ONe more solution to this task
 //const squares = (x, n) => Array.from({ length: n }, (_, index) => x ** (2 ** index))
 
+console.log('---------------------------')
+//N-th Power
+// This kata is from check py.checkio.org
+// You are given an array with positive numbers and a number N. You should find
+// the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
+//
+// Let's look at a few examples:
+//
+// array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
+// array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
 
+function index(array, n){
+   for(let i=0; i<array.length; i++){
+       if(array.length>n){
+           return Math.pow(array[n],n)
+       }
+   }
+   return -1;
+}
+console.log(index([1, 2, 3, 4],2)); //9
+console.log(index([1, 2],3)); //-1
