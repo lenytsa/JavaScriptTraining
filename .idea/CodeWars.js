1785567791e6ddc0aae365bd861c157b3ad958bd
yelenaportnaya <yportnaya@hotmail.com>
2020-09-11 00:26:01 -0700

    Is the given number n a power of 2. Является ли данное число n степенью числа 2? https://app.pasv.us/course/5d1cbf67086fc30038bf1859/lesson/5dcfadebd95082003c2659d5

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -1129,3 +1129,5 @@ function cutCube(volume,n){
     return Math.cbrt(volume / n) % 1 === 0 && Math.cbrt(n) % 1 === 0 ? true : false;
 }
 console.log(cutCube(27,27))
+console.log('=======')
+console.log(Math.sqrt('-81'))
