56f897f84c28dbd38303bd354b90ae2b9ae3aff3
yelenaportnaya <yportnaya@hotmail.com>
2020-09-11 12:51:02 -0700

    CodeWars: Price of Mangoes https://www.codewars.com/kata/57a77726bb9944d000000b06/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -1142,4 +1142,18 @@ function cockroachSpeed(s) {
     return Math.floor(s*100000/3600);
 }
 console.log(cockroachSpeed(1.08))
+console.log('------------------------')
 
+//Price of Mangoes
+// There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity
+// and price (per mango), calculate the total cost of the mangoes.
+//
+// Examples
+// mango(3, 3) ==> 6    # 2 mangoes for 3 = 6; +1 mango for free
+//     mango(9, 5) ==> 30   # 6 mangoes for 5 = 30; +3 mangoes for free
+
+function mango(quantity, price){
+    let q = Math.floor(quantity/3);
+    return (quantity -q) * price;
+}
+console.log(mango(3, 3));
