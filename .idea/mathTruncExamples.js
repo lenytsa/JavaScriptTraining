af3112776a53be99ab891b6b92321ffe61a5083d
yelenaportnaya <yportnaya@hotmail.com>
2020-09-12 18:50:58 -0700

    Formatting a number - n decimal places https://app.pasv.us/course/5d1cbf67086fc30038bf1859/lesson/5dd5dff6b51a0b003c0b5624

--- .idea/mathTruncExamples.js
+++ .idea/mathTruncExamples.js
@@ -14,3 +14,19 @@ function integerPartsOfElements(arr){
     return arrRes;
 }
 console.log(integerPartsOfElements([7.23, 34.89, 0.9, 4.45, 8.5]));
+
+
+// //Форматирование числа - n знаков в дробной части
+// Напишите функцию nDecimalDigits, которая принимает числа num (исходное число) и n (количество десятичных знаков) в качестве аргументов и возвращает число num только с n знаками в дробной части. Число не требуется округлять, просто оставьте только n десятичных знаков.
+//
+//     Примеры:
+//
+// функция nDecimalDigits(6.642867, 3) должна возвратить 6.642, результат округления до трех десятичных знаков 6.643 - будет неверный.
+//
+//     функция nDecimalDigits(4.1567, 2) должна возвратить 4.15, результат округления до двух десятичных знаков 4.16 - будет неверный.
+//
+function nDecimalDigits(num, n){
+    let numPowerConverter = Math.pow(10,n);
+    return Math.trunc(num * numPowerConverter)/ numPowerConverter;
+}
+console.log(nDecimalDigits(4.1567, 3));
