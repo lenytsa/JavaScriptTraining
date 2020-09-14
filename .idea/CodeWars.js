eb9c68c4c5d0f245c893e3fd96390561276ca556
yelenaportnaya <yportnaya@hotmail.com>
2020-09-13 21:48:51 -0700

    CodeWars: Find min and max https://www.codewars.com/kata/57a1ae8c7cb1f31e4e000130/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -1354,5 +1354,10 @@ function pipeFix(numbers){
     return res;
 }
 
+//Find min and max
+//Implement a function that returns the minimal and the maximal value of a list (in this order).
+function getMinMax(arr){
+   return [Math.min(...arr), Math.max(...arr)];
+};
 
 
