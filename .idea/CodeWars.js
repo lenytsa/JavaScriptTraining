a873668c953b9b0e56db025ad1635e1e8be7f657
yelenaportnaya <yportnaya@hotmail.com>
2020-09-15 23:55:09 -0700

    CodeWars: Localize The Barycenter of a Triangle https://www.codewars.com/kata/5601c5f6ba804403c7000004/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -1430,6 +1430,7 @@ function addBinary(a,b) {
 }
 //OR const addBinary = (a, b) => (a + b).toString(2);
 
+console.log('--------------------')
 
 /*
 CodeWars:Convert to Binary
@@ -1444,9 +1445,34 @@ function toBinary(n){
 }
 //OR let toBinary = n => +n.toString(2)
 
-let num3 = '1100100';
-num3 = parseInt(num3, 2);
-num = num3.toString(2);
-let type = typeof num3;
-//console.log(type);
-//console.log((1234567890).toString(10));
+console.log('--------------------')
+
+/*
+Localize The Barycenter of a Triangle
+The medians of a triangle are the segments that unit the vertices with the midpoint of their opposite sides.
+ The three medians of a triangle intersect at the same point, called the barycenter or the centroid.
+ Given a triangle, defined by the cartesian coordinates of its vertices we need to localize its
+  barycenter or centroid.
+
+    The function bar_triang() or barTriang or bar-triang, receives the coordinates of the three vertices
+    A, B and C as three different arguments and outputs the coordinates of the barycenter O in an array [xO, yO]
+
+This is how our asked function should work: the result of the coordinates should be expressed up to four
+ decimals, (rounded result).
+
+You know that the coordinates of the barycenter are given by the following formulas.
+
+    source: imgur.com
+
+For additional information about this important point of a triangle see at: (https://en.wikipedia.org/wiki/Centroid)
+
+                                                                                 Let's see some cases:
+
+barTriang([4, 6], [12, 4], [10, 10]) ------> [8.6667, 6.6667]
+
+barTriang([4, 2], [12, 2], [6, 10]) ------> [7.3333, 4.6667]
+The given points form a real or a degenerate triangle but in each case the above formulas can be used.
+*/
+function barTriang(p1, p2, p3){
+    return [+(((p1[0] +p2[0] + p3[0])/3).toFixed(4)),(+((p1[1] +p2[1] +p3[1])/3).toFixed(4))];
+}
