30534bcbcc499a192473470783c6315d0e48b4b3
yelenaportnaya <yportnaya@hotmail.com>
2020-09-17 16:08:06 -0700

    Arrays. Create an array of n odd numbers where array length = n https://app.pasv.us/course/5d1cbf67086fc30038bf1859/lesson/5d33db260ca4f00038111265

--- .idea/arraysExamples.js
+++ .idea/arraysExamples.js
@@ -92,3 +92,25 @@ function createEvenArray(n){
 console.log(createEvenArray(5));
 console.log(createEvenArray(8));
 console.log(createEvenArray(2));
+
+/*
+Создайте массив из n нечетных чисел
+Напишите функцию createOddArray, которая принимает целое неотрицательное число n в качестве аргумента и возвращает массив, содержащий n нечетных чисел.
+
+    Пример:
+
+функция createOddArray(5) должна возвратить [1, 3, 5, 7, 9];
+функция createOddArray(2) должна возвратить [1, 3];
+функция createOddArray(1) должна возвратить [1];
+функция createOddArray(0) должна возвратить [].*/
+function createOddArray(n){
+    let newArr = [];
+    let i=0;
+    while(newArr.length!==n){
+        if(i%2!==0)
+            newArr.push(i);
+        i++
+    }
+    return newArr
+}
+console.log(createOddArray(5))
