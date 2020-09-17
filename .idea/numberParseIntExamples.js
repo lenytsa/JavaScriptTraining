81e6175e9dc99c653a7e164b9e6e93e6365a3d24
yelenaportnaya <yportnaya@hotmail.com>
2020-09-16 22:19:28 -0700

    Number.parseInt(). Содержит ли строка целое число? https://app.pasv.us/course/5d1cbf67086fc30038bf1859/lesson/5dcfaf6fd95082003c2659f3

--- .idea/numberParseIntExamples.js
+++ .idea/numberParseIntExamples.js
@@ -15,3 +15,28 @@ function getDay(answer){
     return Number.parseInt(answer);
 }
 console.log(getDay("5th of September"));
+
+console.log('---------------')
+/*
+Содержит ли строка целое число?
+    Напишите функцию isStringContainsNumber, которая принимает строку str в качестве аргумента и возвращает true, если строка содержит целое число, и false, в противном случае.
+
+    Указание: строка содержит целое число, если результат нестрогого сравнения исходной строки и преобразования строки к целому числу равен true.
+
+    Например,
+
+'45' == Number.parseInt('45'); (результат сравнения '45' == 45 равен true)
+
+'1z2' == Number.parseInt('1z2'); (результат сравнения '1z2' == 1 равен false)
+
+Примеры:
+
+    функция isStringContainsNumber("123") должна возвратить true.
+    функция isStringContainsNumber("-34") должна возвратить true.
+    функция isStringContainsNumber("s12") должна возвратить false.*/
+function isStringContainsNumber(str){
+    return str==Number.parseInt(str);
+}
+console.log(isStringContainsNumber("123"));
+console.log(isStringContainsNumber("-34"));
+console.log(isStringContainsNumber("s12"));
