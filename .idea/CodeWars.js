1f688e02b84b8d821ab42d7299fa7549678b6a44
yelenaportnaya <yportnaya@hotmail.com>
2020-09-16 23:28:50 -0700

    Number.parseFloat(string).CodeWars:Parse float https://www.codewars.com/kata/57a386117cb1f31890000039/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -1530,3 +1530,11 @@ function binToDec(bin){
     return Number.parseInt(bin ,2);
 }
 console.log('--------------------')
+
+/*
+Parse float
+Write function parseF which takes an input and returns a number or null if
+    conversion is not possible. The input can be one of many different types so be aware.*/
+function parseF(s) {
+    return (Number.isNaN(parseFloat(s)) ? null : parseFloat(s));
+}
