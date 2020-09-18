c713d0493f68aa25f799329f2329867812035ce2
yelenaportnaya <yportnaya@hotmail.com>
2020-09-18 11:39:08 -0700

    CodeWars: Coefficients of the Quadratic Equation https://www.codewars.com/kata/5d59576768ba810001f1f8d6

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -1572,3 +1572,52 @@ function popElement(arr){
 
     return arr;
 }
+
+console.log('--------------------')
+/*
+Series of integers from m to n
+Task
+Write a function that accepts two arguments and generates a sequence containing
+ the integers from the first argument to the second inclusive.
+    Input
+Pair of integers greater than or equal to 0. The second argument will always be greater than or equal to the first.
+    Example
+generateIntegers(2, 5) // --> [2, 3, 4, 5]*/
+function generateIntegers(m, n) {
+    let res =[];
+    for(let i=m;i<=n; i++){
+        res.push(i);
+    }
+    return res;
+}
+console.log(generateIntegers(2, 5));
+
+/*
+Coefficients of the Quadratic Equation
+In this Kata you are expected to find the coefficients of quadratic equation of the given two roots (x1 and x2).
+
+Equation will be the form of ax**2 + bx + c = 0
+
+Return type is a Vector (tuple in Rust, Array in Ruby) containing coefficients of the equations in the order (a, b, c).
+
+    Since there are infinitely many solutions to this problem, we fix a = 1.
+
+Remember, the roots can be written like (x-x1) * (x-x2) = 0
+
+Example
+quadratic(1,2) = (1, -3, 2)
+This means (x-1) * (x-2) = 0; when we do the multiplication this becomes x^2 - 3x + 2 = 0
+
+Example 2
+quadratic(0,1) = (1, -1, 0)
+This means (x-0) * (x-1) = 0; when we do the multiplication this becomes x^2 - x + 0 = 0
+
+Notes
+Inputs will be integers.
+    When x1 == x2, this means the root has the multiplicity of two*/
+function quadratic(x1, x2){
+    let a = 1;
+    let b= a * -x2 + a * -x1;
+    let c = -x1 * -x2;
+    return [1, b, c];
+}
