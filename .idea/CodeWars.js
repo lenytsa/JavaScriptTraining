7b3b3b9fff11e0c4b2009ed15266d8035e52209c
yelenaportnaya <yportnaya@hotmail.com>
2020-09-21 12:32:07 -0700

    CodeWars: Square(n) Sum https://www.codewars.com/kata/515e271a311df0350d00000f/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -1874,3 +1874,18 @@ function findDifference(a, b) {
    return Math.abs(a[0]*a[1]*a[2] - b[0]*b[1]*b[2]);
 }
 console.log(findDifference([2, 2, 3], [5, 4, 1]));
+console.log('-------------------')
+
+/*
+Square(n) Sum
+https://www.codewars.com/kata/square-n-sum
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+    For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.*/
+function squareSum(numbers){
+    let sqSum =0;
+    for(let elm of numbers){
+        sqSum = sqSum + elm**2;
+    }
+    return sqSum;
+}
