62289a4bbc9c8dd52ae92acf00b5d85cbb8eb37e
yelenaportnaya <yportnaya@hotmail.com>
2020-09-21 11:33:24 -0700

    Product of array elements. https://app.pasv.us/course/5d1cbf67086fc30038bf1859/lesson/5dcf5384a9b298003cf84f06

--- .idea/arraySumOrProduct.js
+++ .idea/arraySumOrProduct.js
@@ -17,3 +17,24 @@ function sumOfElements(arr){
     return sum;
 }
 console.log(sumOfElements([1, 2, 3]));
+
+console.log('----------------')
+/*
+Произведение элементов массива
+Напишите функцию productOfElements, которая принимает массив arr в качестве аргумента и возвращает произведение элементов массива.
+
+    Используйте в решении цикл. Не разрешается использование методов массивов.
+
+    Пример:
+
+функция productOfElements([1, 2, 4]) должна возвратить 8;
+функция productOfElements([2, 15]) должна возвратить 30;
+функция productOfElements([]) должна возвратить 0.*/
+function productOfElements(arr){
+    let product = 1;
+    if(arr.length === 0) return 0;
+    for(let elm of arr){
+        product = product * elm;
+    }
+    return product;
+}
