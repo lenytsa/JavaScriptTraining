47b7ef13678f5da0139e4c684c6f211b11c0314a
yelenaportnaya <yportnaya@hotmail.com>
2020-09-20 23:01:38 -0700

    CodeWars: Simple consecutive pairs https://www.codewars.com/kata/5a3e1319b6486ac96f000049/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -1686,6 +1686,7 @@ function countSheeps(arrayOfSheep) {
     }
     return count;
 }
+console.log('-----------------------------')
 
 
 /*
@@ -1723,3 +1724,36 @@ function crap(x, bags, cap){
     }
     return crapCount > totalCap ? 'Cr@p': 'Clean';
 }
+console.log('-----------------------------')
+
+/*
+Simple consecutive pairs
+In this Kata your task will be to return the count of pairs that have consecutive numbers as follows:
+
+pairs([1,2,5,8,-4,-3,7,6,5]) = 3
+The pairs are selected as follows [(1,2),(5,8),(-4,-3),(7,6),5]
+--the first pair is (1,2) and the numbers in the pair are consecutive; Count = 1
+--the second pair is (5,8) and are not consecutive
+--the third pair is (-4,-3), consecutive. Count = 2
+--the fourth pair is (7,6), also consecutive. Count = 3.
+--the last digit has no pair, so we ignore.
+*/
+
+function pairs(ar) {
+    let count = 0;
+    for (let i = 0; i < ar.length; i += 2) {
+        if (ar[i] + 1 === ar[i + 1] || ar[i] === ar[i + 1] + 1) {
+            count++
+        }
+    }
+    return count;
+};
+//OR
+function pairs1(array, count = 0) {
+  for (let i = 0; i < array.length; i += 2)
+    if (Math.abs(array[i] - array[i + 1]) === 1)
+      count += 1;
+  return count;
+}
+console.log(pairs([1,2,5,8,-4,-3,7,6,5]));
+console.log('-----------------------------')
