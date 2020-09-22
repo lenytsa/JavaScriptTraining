3838c735c220428d0fa7d63cc5805d3387e67c83
yelenaportnaya <yportnaya@hotmail.com>
2020-09-21 19:18:30 -0700

    CodeWars: Sum of positive https://www.codewars.com/kata/5715eaedb436cf5606000381/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -1944,3 +1944,19 @@ function number(busStops){
 }
 console.log(number([[10,0],[3,5],[5,8]]));
 
+/*Sum of positive
+https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
+You get an array of numbers, return the sum of all of the positives ones.
+
+    Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.*/
+function positiveSum(arr) {
+    let sum = 0;
+    for(let elm of arr){
+        if(elm > 0){
+            sum += elm;
+        }
+    }
+    return sum;
+}
