85d65e16210076f24766afe573d95ff4d09a8f25
yelenaportnaya <yportnaya@hotmail.com>
2020-09-21 21:06:57 -0700

    CodeWars: Divide and Conquer https://www.codewars.com/kata/57eaec5608fed543d6000021/solutions/javascript

--- .idea/CodeWars-1.js
+++ .idea/CodeWars-1.js
@@ -11,3 +11,24 @@ function find_average(array) {
     return average;
 }
 console.log(find_average([1,1,1]));
+console.log('------------------')
+/*
+Divide and Conquer
+Given a mixed array of number and string representations of integers, add up the string integers and
+subtract this from the total of the non-string integers.
+
+    Return as a number.*/
+function divCon(x){
+    let numbers= 0;
+    let nonNumbers=0;
+    for(let elm of x){
+        if(typeof(elm)==='number'){
+            numbers+=elm;
+        }
+        if(typeof(elm)==='string'){
+            nonNumbers+=Number.parseInt(elm);
+        }
+    }
+    return numbers-nonNumbers;
+}
+console.log(divCon([9, 3, '7', '3']));
