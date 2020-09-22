7862903f95fb1337259ec87497ae05a92b432785
yelenaportnaya <yportnaya@hotmail.com>
2020-09-21 21:27:18 -0700

    CodeWars: Sum of Odd Cubed Numbers https://www.codewars.com/kata/580dda86c40fa6c45f00028a/solutions/javascript/me/best_practice

--- .idea/CodeWars-1.js
+++ .idea/CodeWars-1.js
@@ -32,3 +32,23 @@ function divCon(x){
     return numbers-nonNumbers;
 }
 console.log(divCon([9, 3, '7', '3']));
+console.log('------------------')
+
+/*Sum of Odd Cubed Numbers
+Find the sum of the odd numbers within an array, after cubing the initial integers. The function should
+return undefined/None/nil/NULL if any of the values aren't numbers.*/
+function cubeOdd(arr) {
+    let sumOfOdd=0;
+    for(let elm of arr ){
+        if(elm %2 !==0){
+            sumOfOdd= sumOfOdd + elm**3;
+        }
+        if(typeof(elm)== 'string' ){
+            return undefined;
+        }
+    }
+    return sumOfOdd
+}
+console.log(cubeOdd([1, 2, 3, 4]));
+console.log(cubeOdd(["a",12,9,"z",42]));
+console.log(cubeOdd(['a', 12, 9, 'z', 42]));
