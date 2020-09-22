f111fbc16e8e59c19d08c4bb7c9c75bd4099407a
yelenaportnaya <yportnaya@hotmail.com>
2020-09-21 23:42:32 -0700

    CodeWars: Row Weights https://www.codewars.com/kata/5abd66a5ccfd1130b30000a9/solutions/javascript

--- .idea/CodeWars-1.js
+++ .idea/CodeWars-1.js
@@ -140,3 +140,40 @@ function oddOrEven(array) {
     if(sum %2 === 0) return 'even';
     return 'odd';
 }
+console.log('----------------')
+
+/*Row Weights
+Scenario
+Several people are standing in a row divided into two teams. The first person goes into team 1, the second goes
+into team 2, the third goes into team 1, and so on.
+
+    Task
+Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where
+the first one is the total weight of team 1,
+    and the second one is the total weight of team 2.
+Notes
+Array size is at least 1.
+All numbers will be positive.
+    Input >> Output Examples
+rowWeights([13, 27, 49])  ==>  return (62, 27)
+Explanation:
+    The first element 62 is the total weight of team 1, and the second element 27 is the total weight of team 2.
+    rowWeights([50, 60, 70, 80])  ==>  return (120, 140)
+
+ */
+function rowWeights(array) {
+    let sT1 = 0;
+    let sT2 = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (i % 2 === 0) {
+            sT1 += array[i];
+        }
+        if (i % 2 !== 0) {
+            sT2 += array[i];
+        }
+    }
+    return [sT1, sT2];
+}
+console.log(rowWeights([100,50]));
+console.log(rowWeights([50,60,70,80]));
+console.log(rowWeights([0,1,0]));
