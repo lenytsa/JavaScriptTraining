8b7775c05657af5a87f3568401ec2f38fbae913f
yelenaportnaya <yportnaya@hotmail.com>
2020-09-30 21:52:34 -0700

    CodeWars: Sum without highest and lowest number https://www.codewars.com/kata/576b93db1129fcf2200001e6/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -2056,3 +2056,30 @@ function removeSmallest1(arr) {
 console.log(removeSmallest([4,2,3,1,4,5]));
 console.log(removeSmallest1([4,2,3,1,4,5]));
 
+console.log('---------------------------')
+/*
+Sum without highest and lowest number
+Sum all the numbers of the array (in F# and Haskell you get a list) except the highest and the lowest element
+(the value, not the index!).
+(The highest/lowest element is respectively only one element at each edge, even if there are more than one with
+    the same value!)
+
+Example:
+
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+If array is empty, null or None, or if only 1 Element exists, return 0.
+Note:In C++ instead null an empty vector is used. In C there is no null. ;-)
+*/
+function sumArray(array) {
+    if (array === null || array.length == 0 || array.length == 1){return 0}
+    let min = Math.min(...array);
+    let max = Math.max(...array);
+    let sum =0;
+    for(let i= 0; i<array.length; i++){
+        sum = sum + array[i]
+    }
+    return sum-max-min;
+}
+console.log((sumArray([ 6, 2, 1, 8, 10 ])));
+
