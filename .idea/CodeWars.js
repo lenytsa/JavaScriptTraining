a9c55946ce9c1c1518e1a275036d5f1a491b4420
yelenaportnaya <yportnaya@hotmail.com>
2020-10-01 00:15:56 -0700

    CodeWars: Basic Training: Add item to an Array. https://app.pasv.us/course/5d1cbf67086fc30038bf1859/lesson/5dcf72ced95082003c26560e

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -2127,3 +2127,15 @@ function maxDiff(list) {
 function maxDiff(list) {
     return list.length > 1 ? Math.max(...list) - Math.min(...list) : 0
 }
+console.log('---------------------------')
+
+/*Basic Training: Add item to an Array
+https://www.codewars.com/kata/511f0fe64ae8683297000001/train/javascript
+Add the value "codewars" to the websites array.
+    After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+    var websites = [];*/
+websites.push('codewars');
+
