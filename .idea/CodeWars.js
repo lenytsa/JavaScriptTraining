ecacfae3d757f5d3e27a85606aa5d8188e39283e
yelenaportnaya <yportnaya@hotmail.com>
2020-09-30 21:32:35 -0700

    CodeWars: Remove the minimum https://www.codewars.com/kata/remove-the-minimum

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -2014,3 +2014,45 @@ var gimme = function (inputArray) {
 };
 console.log(gimme([2, 3, 1]));
 console.log(gimme([5,10,14]));
+console.log('------------------')
+
+/*Remove the minimum
+The museum of incredible dull things
+The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect,
+    comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one
+with the lowest rating.
+
+    However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write' +
+' a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple
+elements with the same value, remove the one with a lower index. If you get an empty array/list, return
+an empty array/list.
+
+    Don't change the order of the elements that are left.
+
+Examples
+removeSmallest([1,2,3,4,5]) = [2,3,4,5]
+removeSmallest([5,3,2,1,4]) = [5,3,2,4]
+removeSmallest([2,2,1,2,1]) = [2,2,2,1]
+
+Task*/
+//this solution is not acceptable on codewars it says it mutates the original array/list.
+function removeSmallest(numbers) {
+    arr = Array.from(numbers);
+    arr.sort((a, b) => a - b);
+    let ind = arr[0];
+    let indInOrig = numbers.indexOf(ind);
+    //The splice() method changes the contents of an array by removing existing elements and/or adding new elements.
+    //  The second parameter of splice is the number of elements to remove
+    numbers.splice(indInOrig, 1);
+    return numbers;
+}
+//This solution is correct one
+function removeSmallest1(arr) {
+    arr = arr.slice(); //copy the array
+    arr.splice( arr.indexOf(Math.min.apply(null, arr)),1)
+    return arr;
+}
+console.log(removeSmallest([4,2,3,1,4,5]));
+console.log(removeSmallest1([4,2,3,1,4,5]));
+
