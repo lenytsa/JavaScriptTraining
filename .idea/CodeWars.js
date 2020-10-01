aae05bfb3aabeab44f00f6c3a437de73b9d42fda
yelenaportnaya <yportnaya@hotmail.com>
2020-10-01 16:05:50 -0700

    CodeWars: Basic Sequence Practice https://www.codewars.com/kata/5436f26c4e3d6c40e5000282/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -2148,3 +2148,39 @@ You are trying to put a hash in ruby or an object in javascript or java into an 
 items = []
 items.push({a: "b", c: "d"});
 console.log(items);
+console.log('---------------------------')
+
+/*
+Basic Sequence Practice
+A sequence or a series, in mathematics, is a string of objects, like numbers, that follow a particular pattern.
+    The individual elements in a sequence are called terms. A simple example is 3, 6, 9, 12, 15, 18, 21, ..., where
+the pattern is: "add 3 to the previous term".
+
+    In this kata, we will be using a more complicated sequence: 0, 1, 3, 6, 10, 15, 21, 28, ... This sequence is
+generated with the pattern: "the nth term is the sum of numbers from 0 to n, inclusive".
+
+    [ 0,  1,    3,      6,   ...]
+0  0+1  0+1+2  0+1+2+3*/
+function sumOfN(n) {
+    let resP = [];
+    let resN = [];
+    let sum = 0;
+    if(n===0) return [0];
+    if(n < 0) {
+        n = Math.abs(n);
+        for (let i = 0; i <= n; i++) {
+            sum = sum + (-i);
+            resN.push(sum)
+        }
+        return resN;
+    }if(n > 0) {
+        for (let i = 0; i <= n; i++) {
+            sum = sum + i
+            resP.push(sum)
+        }
+        return resP;
+    }
+}
+console.log(sumOfN(-3));
+console.log(sumOfN(4));
+console.log(sumOfN(0));
