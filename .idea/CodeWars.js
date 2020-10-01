626711219d86e06a35a84bf927ffb9118917ceec
yelenaportnaya <yportnaya@hotmail.com>
2020-10-01 00:25:49 -0700

    CodeWars: Push a hash/an object into array https://www.codewars.com/kata/527b3cd0492b6b15250060af/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -2137,5 +2137,14 @@ Add the value "codewars" to the websites array.
 The websites array has already been defined for you using the following code:
 
     var websites = [];*/
+var websites = []
 websites.push('codewars');
+console.log('---------------------------')
 
+/*
+Push a hash/an object into array
+You are trying to put a hash in ruby or an object in javascript or java into an array,
+    but it always returns error, solve it and keep it as simple as possible!*/
+items = []
+items.push({a: "b", c: "d"});
+console.log(items);
