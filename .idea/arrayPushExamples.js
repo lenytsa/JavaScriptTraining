f4d45f4284fcbde32239c479f7ff03ad0c647da8
yelenaportnaya <yportnaya@hotmail.com>
2020-09-30 22:52:47 -0700

    Новая длина массива. New array length, cannot use length https://app.pasv.us/course/5d1cbf67086fc30038bf1859/lesson/5dcf72ced95082003c26560e

--- .idea/arrayPushExamples.js
+++ .idea/arrayPushExamples.js
@@ -12,3 +12,26 @@ function addValue(arr, value){
     return arr;
 }
 console.log(addValue([1, 2, 3], 10));
+console.log('---------------------------');
+
+/*
+Новая длина массива
+Напишите функцию addValue, которая принимает массив arr и величину value в качестве аргументов,
+    добавляет величину value в конец массива и возвращает новую длину массива. В этой задаче нельзя использовать
+свойство length.
+
+    Пример:
+
+функция addValue([1, 2, 3], 10) должна возвратить 4;
+функция addValue([1, -1, 14, 0], "a") должна возвратить 5;
+функция addValue([], 10) должна возвратить 1.*/
+function addValue(arr, value){
+    arr.push(value);
+    let count =0;
+    for(let elm of arr){
+        arr[elm];
+        count++
+    }
+    return count;
+}
+console.log(addValue([1, 2, 3,5], 10));
