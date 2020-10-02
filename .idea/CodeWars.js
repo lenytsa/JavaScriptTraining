0bbe012fc09efbddcea2dd1cfdf1e40c694a7401
yelenaportnaya <yportnaya@hotmail.com>
2020-10-01 21:24:24 -0700

    CodeWars: Pre-FizzBuzz Workout #1 https://www.codewars.com/kata/pre-fizzbuzz-workout-number-1

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -2249,3 +2249,21 @@ function generateRange(min, max, step) {
     return res;
 }
 console.log(generateRange(2, 10, 2));
+console.log('---------------------------')
+
+/*Pre-FizzBuzz Workout #1
+This is the first step to understanding FizzBuzz.
+
+    Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over
+its value.
+
+    Your expected output is an array of positive integers from 1 to n (inclusive).
+
+    Your job is to write an algorithm that gets you from the input to the output.*/
+function preFizz(n) {
+    let res = [];
+    for (let i = 1; i <= n; i++) {
+        res.push(i);
+    }
+    return res;
+}
