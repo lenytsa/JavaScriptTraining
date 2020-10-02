e12ddf328b374b378947ddd593b7049a137ae86c
yelenaportnaya <yportnaya@hotmail.com>
2020-10-02 12:02:55 -0700

    CodeWars: Show multiples of 2 numbers within a range https://www.codewars.com/kata/583989556754d6f4c700018e/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -2286,3 +2286,25 @@ function powersOfTwo(n){
     return res;
 }
 console.log(powersOfTwo(4));
+console.log('---------------------------')
+
+/*Show multiples of 2 numbers within a range
+Print all numbers up to 3rd parameter which are multiple of both 1st and 2nd parameter.
+
+    Python, Javascript, Java versions: return results in a list/array
+
+NOTICE:
+
+    Do NOT worry about checking zeros or negative values.
+    To find out if 3rd parameter (the upper limit) is inclusive or not, check the tests, it differs in each translation*/
+// Javascript: return multiples into an array
+function multiples(s1,s2,s3){
+    let res =[];
+    for(let i= 1; i<s3; i++){
+        if(i%s1 === 0 && i%s2 ===0 ) {
+            res.push(i);
+        }
+    }
+    return res;
+}
+console.log(multiples(2,4,40));
