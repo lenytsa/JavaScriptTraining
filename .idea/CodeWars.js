8034012be842a852d0fc43b4918e54a729cf0c6a
yelenaportnaya <yportnaya@hotmail.com>
2020-10-03 14:44:50 -0700

    CodeWars: Reversed sequence https://www.codewars.com/kata/5a00e05cc374cb34d100000d/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -2385,3 +2385,17 @@ function reverse(array) {
     return res;
 }
 console.log( reverse([1,2,3]));
+
+/*
+Reversed sequence
+Get the number n (n>0) to return the reversed sequence from n to 1.
+
+Example : n=5 >> [5,4,3,2,1]*/
+function reverseSeq(n)  {
+    let res = [];
+    for (let i=n; i>0; i--) {
+        res.push(i);
+    }
+    return res;
+}
+console.log(reverseSeq(5));
