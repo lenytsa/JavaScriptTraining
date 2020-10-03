9f1e18052d70edb99a547082823f33b0d4df23eb
yelenaportnaya <yportnaya@hotmail.com>
2020-10-02 23:26:22 -0700

    CodeWars: String doubles https://www.codewars.com/kata/5a145ab08ba9148dd6000094/train/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -2331,7 +2331,6 @@ Your task is to process a string with "#" symbols.
 "#######"       ==>  ""
 ""              ==>  ""*/
 function cleanString(s) {
-    //s=s.split``;
     let res=[];
     for(let i=0; i<s.length; i++){
         if(s[i]==='#'){
@@ -2343,3 +2342,32 @@ function cleanString(s) {
     return res.join('');
 }
 console.log(cleanString('abc#d##c'));//ac
+console.log('------------------------');
+
+/*
+String doubles
+In this Kata, you will write a function doubles that will remove double string characters that are adjacent to
+each other.
+
+    For example:
+
+    doubles('abbcccdddda') = 'aca', because, from left to right:
+    a) There is only one 'a' on the left hand side, so it stays.
+    b) The 2 b's disappear because we are removing double characters that are adjacent.
+c) Of the 3 c's, we remove two. We are only removing doubles.
+d) The 4 d's all disappear, because we first remove the first double, and again we remove the second double.
+Two more examples: doubles('abbbzz') = 'ab' and doubles('abba') = "". In the second example, when we remove the b's
+in 'abba', the double a that results is then removed.
+
+The strings will contain lowercase letters only. More examples in the test cas*/
+function doubles(s) {
+    const cs = [];
+    for (let letter of s) {
+        if (cs.length != 0 && cs[cs.length - 1] == letter)
+            cs.pop();
+        else
+            cs.push(letter);
+    }
+    return cs.join('');
+}
+console.log(doubles('abbbzz'));//ab
