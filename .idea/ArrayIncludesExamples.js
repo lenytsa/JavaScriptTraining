52c9daf6e615a256f553a99cd869c81ad88b38e6
yelenaportnaya <yportnaya@hotmail.com>
2020-10-03 22:08:07 -0700

    Does the array contain an element that is one more than the minimum element of the given array? https://app.pasv.us/course/5d1cbf67086fc30038bf1859/lesson/5d33db930ca4f00038111267

--- .idea/ArrayIncludesExamples.js
+++ .idea/ArrayIncludesExamples.js
@@ -15,3 +15,23 @@ function includeZero(arr) {
 
 console.log(includeZero([1, 10, 2, -3, 8]));
 console.log(includeZero([0, 1, 2]));
+console.log('-----------------');
+
+/*Содержит ли массив элемент, который на единицу больше минимального элемента данного массива?
+    Напишите функцию includeGreaterThanMin, которая принимает непустой массив arr в качестве аргумента, и возвращает
+true, если массив содержит элемент, который на единицу больше минимального элемента, и false - в противном случае.
+
+    Пример:
+
+функция includeGreaterThanMin([1, 3, 4, 5]) должна возвратить false, так как минимальный элемент равен 1, и массив
+не содержит 2;
+функция includeGreaterThanMin([0, 1, 2, 3]) должна возвратить true, так как минимальный элемент равен 0, и массив
+содержит 1;
+функция includeGreaterThanMin([9, -2, 10, -1, 8]) должна возвратить true, так как минимальный элемент равен -2, и
+массив содержит -1.*/
+function includeGreaterThanMin(arr){
+    let min = Math.min(...arr);
+    return arr.includes(min +1);
+}
+console.log(includeGreaterThanMin([1, 3, 4, 5])); //false
+console.log(includeGreaterThanMin([0, 1, 2, 3]));//true
