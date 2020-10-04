6e899cc8648e9753938dd2af4a440bc21858cebf
yelenaportnaya <yportnaya@hotmail.com>
2020-10-03 22:19:28 -0700

    CodeWars: No Loops 2 - You only need one https://www.codewars.com/kata/57cc40b2f8392dbf2a0003ce/solutions/javascript

--- .idea/CodeWars-2.js
+++ .idea/CodeWars-2.js
@@ -30,3 +30,19 @@ Enumerable Magic #3 - Does My List Include This?
 function include(arr, item){
     return arr.includes(item);
 }
+
+console.log('-----------------------------');
+/*
+No Loops 2 - You only need one
+* No Loops Allowed *
+
+You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains
+the value, without using a loop.
+
+    Array can contain numbers or strings. X can be either. Return true if the array contains the value, false
+if not. With strings you will need to account for case.
+
+Looking for more, loop-restrained fun? Check out the other kata in the series:*/
+function check(a,x){
+    return a.includes(x);
+};
