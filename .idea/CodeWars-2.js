ff112ac851357e2ef9a2fa3482cf9a687938a94a
yelenaportnaya <yportnaya@hotmail.com>
2020-10-03 22:33:55 -0700

    Функция, которая всегда возвращает число 3 https://app.pasv.us/course/5d1cbf67086fc30038bf1859/lesson/5d33dbbb0ca4f00038111268

--- .idea/CodeWars-2.js
+++ .idea/CodeWars-2.js
@@ -46,3 +46,5 @@ Looking for more, loop-restrained fun? Check out the other kata in the series:*/
 function check(a,x){
     return a.includes(x);
 };
+console.log('-----------------------------');
+
