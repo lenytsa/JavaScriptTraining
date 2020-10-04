f0f65900bd90377ab6b04b5099568a3c5473d7a3
yelenaportnaya <yportnaya@hotmail.com>
2020-10-04 14:40:38 -0700

    Codewars: A wolf in sheep's clothing https://www.codewars.com/kata/5c8bfa44b9d1192e1ebd3d15/solutions/javascript/all/best_practice

--- .idea/CodeWars-2.js
+++ .idea/CodeWars-2.js
@@ -89,3 +89,29 @@ If all rooms are busy, return 'None available!'.*/
 function meeting(x){
     return (x.includes('O')) ? x.indexOf('O') : 'None available!';
 }
+console.log('-------------------');
+
+/*
+A wolf in sheep's clothing
+Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend
+to be sheep. Fortunately,
+    you are good at spotting them.
+
+    Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front
+of the queue which is at the end of the array:
+    [sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+
+Note: there will always be exactly one wolf in the array.
+warnTheSheep(["sheep", "sheep", "sheep", "wolf", "sheep"]) === "Oi! Sheep number 1! You are about to be eaten by a wolf!"
+
+warnTheSheep(["sheep", "sheep", "wolf"]) === "Pls go away and stop eating my sheep"
+*/
+function warnTheSheep(queue) {
+    let indexWolf= queue.indexOf('wolf');
+    if(indexWolf === queue.length-1) return 'Pls go away and stop eating my sheep';
+    else
+        return `Oi! Sheep number ${queue.length-1-indexWolf}! You are about to be eaten by a wolf!`
+}
+console.log(warnTheSheep(["sheep", "sheep", "sheep", "wolf", "sheep"]));
+console.log(warnTheSheep(["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep" ]));
