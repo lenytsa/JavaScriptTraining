909b3c8d71633ece8d72779d833d035dfaa0092b
yelenaportnaya <yportnaya@hotmail.com>
2020-10-03 23:45:09 -0700

    Codewars: Be Concise IV - Index of an element in an array. https://www.codewars.com/kata/5703c093022cd1aae90012c9/solutions/javascript

--- .idea/CodeWars-2.js
+++ .idea/CodeWars-2.js
@@ -62,3 +62,15 @@ function findNeedle(haystack) {
     let index = haystack.indexOf('needle');
    return `found the needle at position ${index}`
 }
+
+console.log('-------------------');
+/*
+Be Concise IV - Index of an element in an array
+Task
+Provided is a function find which accepts two parameters in the following order: array, element and
+returns the index of the element if found and "Not found" otherwise. Your task is to shorten the code as much
+as possible in order to meet the strict character count requirements of the Kata. (no more than 85)
+You may assume that all array elements are unique.*/
+function find(a, el) {
+    return a.includes(el)? a.indexOf(el): 'Not found'
+}
