69d8f0fa7116e531693b69d31a9397383c16340e
yelenaportnaya <yportnaya@hotmail.com>
2020-10-03 23:50:01 -0700

    Codewars: The Office IV - Find a Meeting Room https://www.codewars.com/kata/57f604a21bd4fe771b00009c/solutions/javascript/me/best_practice

--- .idea/CodeWars-2.js
+++ .idea/CodeWars-2.js
@@ -74,3 +74,18 @@ You may assume that all array elements are unique.*/
 function find(a, el) {
     return a.includes(el)? a.indexOf(el): 'Not found'
 }
+console.log('-------------------');
+
+/*The Office IV - Find a Meeting Room
+Your job at E-Corp is both boring and difficult. It isn't made any easier by the fact that everyone ' +
+'constantly wants to have a meeting with you, and that the meeting rooms are always taken!
+
+In this kata, you will be given an array. Each value represents a meeting room. Your job? Find the
+first empty one and return its index (N.B. There may be more than one empty room in some test cases).
+
+'X' --> busy 'O' --> empty
+
+If all rooms are busy, return 'None available!'.*/
+function meeting(x){
+    return (x.includes('O')) ? x.indexOf('O') : 'None available!';
+}
