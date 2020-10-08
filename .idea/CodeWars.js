257d3336b5d9adf8a740058bec9cedf66bf3f780
yelenaportnaya <yportnaya@hotmail.com>
2020-10-08 11:58:34 -0700

    CodeWars: Filter out the geese https://www.codewars.com/kata/57ee4a67108d3fd9eb0000e7/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -2512,4 +2512,31 @@ function remove(arrY) {
     return res;
 }
 console.log(remove(['Hello', 'Goodbye', 'Hello Again']));
-console.log(remove([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+console.log(remove([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+/*Filter out the geese
+Write a function, gooseFilter / goose-filter / goose_filter /GooseFilter, that takes an array of strings
+as an argument and returns a filtered array containing the same elements but with the 'geese' removed.
+
+    The geese are any strings in the following array, which is pre-populated in your solution:
+    geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+For example, if this array were passed as an argument:
+    ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+Your function would return the following array:
+    ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+The elements in the returned array should be in the same order as in the initial array passed to your
+function, albeit with the 'geese' removed. Note that all of the strings
+will be in the same case as those provided, and some elements may be repeated.*/
+
+function gooseFilter (birds) {
+    const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter(el =>!geese.includes(el) );
+};
+console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]));
+//[ 'Mallard', 'Hook Bill', 'Crested', 'Blue Swedish' ]
+//OR other solution
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return gooseFilter.pop(geese);
+    // return an array containing all of the strings in the input array except those that match strings in geese
+};
