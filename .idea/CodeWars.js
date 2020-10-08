c6ef97e803e5c7b6b61b419dae771c0f47dad764
yelenaportnaya <yportnaya@hotmail.com>
2020-10-08 14:28:52 -0700

    CodeWars: Find the lucky numbers https://www.codewars.com/kata/580435ab150cca22650001fb/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -2560,3 +2560,17 @@ function fil(word) {
     }
 }
 console.log(well(['good', 'bad', 'bad', 'bad', 'bad']));
+console.log('------------------------------------------')
+
+/*Find the lucky numbers
+Write a function filterLucky/filter_lucky() that accepts a list of integers and filters the list to only include
+the elements that contain the digit 7.
+
+For example,
+
+ghci> filterLucky [1,2,3,4,5,6,7,68,69,70,15,17]
+    [7,70,17]*/
+var filterLucky=x=>{
+    return x.filter(el => (el +'').includes(7));
+}
+console.log( filterLucky([1,2,3,4,5,6,7,68,69,70,15,17]));
