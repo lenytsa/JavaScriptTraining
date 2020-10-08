aef117b7097b7441cb3fafb4d75024c7585ba102
yelenaportnaya <yportnaya@hotmail.com>
2020-10-07 20:52:01 -0700

    CodeWars: List Filtering https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -2412,3 +2412,27 @@ The solution would work like the following:
 function getEvenNumbers(numbersArray){
     return numbersArray.filter(el=>el%2===0);
 }
+//Or
+function getEvenNumbers(numbersArray){
+    return numbersArray.filter(function(num){return !(num % 2) })  // 0 is falsy
+}
+console.log(getEvenNumbers([2,4,5,6]));
+console.log('-------------')
+
+/*
+List Filtering
+In this kata you will create a function that takes a list of non-negative integers and strings and
+returns a new list with the strings filtered out.
+
+    Example
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]*/
+function filter_list(l) {
+    return l.filter(el=>el >=0 && typeof (el)==='number');
+}
+//OR
+function filter_list(l) {
+    return l.filter(function(v) {return typeof v == 'number'})
+}
+console.log(filter_list([1,2,'a','b']));
