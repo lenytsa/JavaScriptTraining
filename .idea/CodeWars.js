ebaa49b0b6c2359b3293487e09e38147ab12113f
yelenaportnaya <yportnaya@hotmail.com>
2020-10-07 21:16:26 -0700

    CodeWars: Enumerable Magic #5- True for Just One? https://www.codewars.com/kata/54599705cbae2aa60b0011a4/train/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -2436,3 +2436,20 @@ function filter_list(l) {
     return l.filter(function(v) {return typeof v == 'number'})
 }
 console.log(filter_list([1,2,'a','b']));
+console.log('------------------------------------------')
+
+/*Enumerable Magic #5- True for Just One?
+    Task
+    Create a function called one that accepts two params:
+
+    a sequence
+a function
+and returns true only if the function in the params returns true for exactly one (1) item in the sequence.
+
+    Example
+one([1, 3, 5, 6, 99, 1, 3], bigger_than_ten) -> true
+one([1, 3, 5, 6, 99, 88, 3], bigger_than_ten) -> false
+one([1, 3, 5, 6, 5, 1, 3], bigger_than_ten) -> false*/
+function one(arr, fun){
+   return arr.filter(el=> fun(el)).length===1;
+}
