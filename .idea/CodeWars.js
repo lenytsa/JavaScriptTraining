5ee6f00b968262b0de0855116b6d40e4656456ac
yelenaportnaya <yportnaya@hotmail.com>
2020-10-13 23:27:03 -0700

    CodeWars: Array.diff https://www.codewars.com/kata/523f5d21c841566fde000009/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -2608,3 +2608,22 @@ console.log(output); // --> ['word', 'word']*/
 function filterEvenLengthWords(words) {
    return words.filter(el=>el.length%2===0);
 }
+console.log('-----------------------------------------');
+
+/*
+
+Array.diff
+Your goal in this kata is to implement a difference function, which subtracts one list from another and
+returns the result.
+
+    It should remove all values from list a, which are present in list b.
+
+arrayDiff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+
+    arrayDiff([1,2,2,2,3],[2]) == [1,3]*/
+function arrayDiff(a, b) {
+    return a.filter(el=>!(b.includes(el)));
+}
+console.log(arrayDiff([1,2],[1]));//[ 2 ]
+console.log(arrayDiff([1,2,2,2,3],[2])); //[ 1, 3 ]
