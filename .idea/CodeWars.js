c36bb2244f371bd16d2d4712bd4cbf88f01a7063
yelenaportnaya <yportnaya@hotmail.com>
2020-10-13 23:18:10 -0700

    CodeWars: filterEvenLengthWords https://www.codewars.com/kata/59564f3bcc15b5591a00004a/solutions/javascript

--- .idea/CodeWars.js
+++ .idea/CodeWars.js
@@ -2593,3 +2593,18 @@ function vowelFilter (letters) {
    return letters.filter(el=>!(vowels.includes(el)));
 };
 console.log(vowelFilter(["i", "s", "e", "a", "w", "e", "m"]));
+console.log('------------------');
+/*
+
+filterEvenLengthWords
+Write a function called "filterEvenLengthWords".
+
+    Given an array of strings, "filterEvenLengthWords" returns an array containing only the elements of the
+given array whose length is an even number.
+
+    var output = filterEvenLengthWords(['word', 'words', 'word', 'words']);
+
+console.log(output); // --> ['word', 'word']*/
+function filterEvenLengthWords(words) {
+   return words.filter(el=>el.length%2===0);
+}
