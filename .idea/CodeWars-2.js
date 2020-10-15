b641b87645edd227469f7d04e38753498b8560c9
yelenaportnaya <yportnaya@hotmail.com>
2020-10-14 19:50:34 -0700

    CodeWars: Array element parity https://www.codewars.com/kata/5a092d9e46d843b9db000064/javascript

--- .idea/CodeWars-2.js
+++ .idea/CodeWars-2.js
@@ -115,3 +115,25 @@ function warnTheSheep(queue) {
 }
 console.log(warnTheSheep(["sheep", "sheep", "sheep", "wolf", "sheep"]));
 console.log(warnTheSheep(["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep" ]));
+
+console.log('-------------------------------');
+/*Array element parity
+In this Kata, you will be given an array of integers whose elements have both a negative and a positive value, except for one integer that is either only negative or only positive. Your task will be to find that integer.
+
+    Examples:
+
+[1, -1, 2, -2, 3] => 3
+
+3 has no matching negative appearance
+
+    [-3, 1, 2, 3, -1, -4, -2] => -4
+
+-4 has no matching positive appearance
+
+    [1, -1, 2, -2, 3, 3] => 3
+
+(the only-positive or only-negative integer may appear more than once)*/
+function solve(arr){
+    return arr.find((elm)=>!arr.includes(-elm));
+};
+console.log(solve([1,-1,2,-2,3,1]));
