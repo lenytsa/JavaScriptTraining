3b00cdc5c168811e381a7d5c037245dc60dc8983
yelenaportnaya <yportnaya@hotmail.com>
2020-10-15 21:47:10 -0700

    CodeWars: Train to remove duplicates from an array with filter() https://www.codewars.com/kata/58308360aeb69a460b0002b2/solutions/javascript

--- .idea/CodeWars-2.js
+++ .idea/CodeWars-2.js
@@ -213,3 +213,28 @@ function completeSeries(arr) {
 console.log(completeSeries([1,4,4,6]));
 console.log(completeSeries([1,4,6]));
 console.log(completeSeries([3,4,5])); //[ 0, 1, 2, 3, 4, 5 ]
+
+console.log('---------------------------------');
+/*
+Train to remove duplicates from an array with filter()
+    In this kata your task is to remove all the duplicates from the array using a standart build-in method - Array.prototype.filter(); return the array containing unique values only.
+
+    Tip: use the index of value(s) to solve this kata
+
+If you are not familiar with filter() - info is here:
+
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+
+        For example:
+
+    var arr = [4];
+
+unique(arr); // should return [4]
+
+var arr = [1,1,1,2,2,3];
+
+unique(arr); // should return [1,2,3]*/
+function unique(arr) {
+    return arr.filter((el, i) => i === arr.indexOf(el));
+}
+console.log(unique([1,2,2,3]));
