5d8b648319f8721b58e1484cb540b1b57096a82c
yelenaportnaya <yportnaya@hotmail.com>
2020-10-16 12:01:37 -0700

    CodeWars: Sort array by string length https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c/solutions/javascript

--- .idea/CodeWars-2.js
+++ .idea/CodeWars-2.js
@@ -415,3 +415,24 @@ function sumTwoSmallestNumbers(numbers) {
     return numbers[0] + numbers[1];
 }
 console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]));
+
+/*
+Sort array by string length
+Write a function that takes an array of strings as an argument and returns a sorted array containing the same
+strings, ordered from shortest to longest.
+
+    For example, if this array were passed as an argument:
+
+    ["Telescopes", "Glasses", "Eyes", "Monocles"]
+
+Your function would return the following array:
+
+    ["Eyes", "Glasses", "Monocles", "Telescopes"]
+
+All of the strings in the array passed to your function will be different lengths, so you will not have to decide
+how to order multiple
+strings of the same length.*/
+function sortByLength (array) {
+   return  array.sort((a, b) => a.length - b.length)
+};
+console.log(sortByLength(["Telescopes", "Glasses", "Eyes", "Monocles"]));//[ 'Eyes', 'Glasses', 'Monocles', 'Telescopes' ]
