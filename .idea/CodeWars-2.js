670c9a04bce40718183495f89336119fb4637a19
yelenaportnaya <yportnaya@hotmail.com>
2020-10-15 21:34:30 -0700

    CodeWars: Complete Series https://www.codewars.com/kata/580a4001d6df740d61000301/solutions/javascript

--- .idea/CodeWars-2.js
+++ .idea/CodeWars-2.js
@@ -178,3 +178,38 @@ function longest(s1, s2) {
     return s3.filter((el, i) => i === s3.indexOf(el)).sort().join('');
 }
 console.log(longest("aretheyhere", "yestheyarehere"));
+console.log('-----------------------------');
+
+/*
+Complete Series
+You are given an array of non-negative integers, your task is to complete the series from 0 to the highest
+number in the array.
+
+    If the numbers in the sequence provided are not in order you should order them, but if a value repeats,
+    then you must return a sequence with only one item, and the value of that item must be 0. like this:
+
+inputs        outputs
+    [2,1]     ->  [0,1,2]
+    [1,4,4,6] ->  [0]
+Notes: all numbers are positive integers.
+
+    This is set of example outputs based on the input sequence.
+
+    inputs        outputs
+    [0,1]   ->    [0,1]
+    [1,4,6] ->    [0,1,2,3,4,5,6]
+    [3,4,5] ->    [0,1,2,3,4,5]
+    [0,1,0] ->    [0]*/
+function completeSeries(arr) {
+    let dup = arr.filter((elm, i) =>arr.indexOf(elm) !== arr.lastIndexOf(elm));
+    let res=[];
+    if(dup.length>0) return [0];
+    let max = Math.max(...arr);
+    for(let i= 0; i<=max; i++){
+        res.push(i);
+    }
+    return res;
+}
+console.log(completeSeries([1,4,4,6]));
+console.log(completeSeries([1,4,6]));
+console.log(completeSeries([3,4,5])); //[ 0, 1, 2, 3, 4, 5 ]
