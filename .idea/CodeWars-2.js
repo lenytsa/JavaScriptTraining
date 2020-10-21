fd9fc9707fb2a9af85ecf5882ced45bc1cd6c392
yelenaportnaya <yportnaya@hotmail.com>
2020-10-21 01:04:09 -0700

    CodeWars: Enumerable Magic #1 - True for All? -every() https://www.codewars.com/kata/54598d1fcbae2ae05200112c/solutions/javascript

--- .idea/CodeWars-2.js
+++ .idea/CodeWars-2.js
@@ -680,3 +680,17 @@ console.log(toCsvText( [[ 0, 1, 2, 3, 4 ],
     [ 10,11,12,13,14 ],
     [ 20,21,22,23,24 ],
     [ 30,31,32,33,34 ]] ));
+console.log('------------------------------------');
+
+/*
+Enumerable Magic #1 - True for All?
+                                   Create a method all which takes an array and a predicate (function pointer),
+    and returns true if the predicate returns true for every element in the array. Otherwise, it should return false.
+    If the array is empty, it should return true, since technically nothing failed the test.
+
+    Here's a (Ruby) resource if you get stuck: http://www.rubycuts.com/enum-all*/
+function all( arr, fun ){
+    return arr.every(fun);
+}
+console.log(all([1,2,3,4,5], function(v){return v<9})); //true
+console.log(all([1,2,3,4,5], function(v){return v>9})); //false
