66083af8d290a4499a1d509b444d7269d2fa60d4
yelenaportnaya <yportnaya@hotmail.com>
2020-10-21 00:06:44 -0700

    CodeWars: Sentence Smash -join() https://www.codewars.com/kata/53dc23c68a0c93699800041d/solutions/javascript

--- .idea/CodeWars-2.js
+++ .idea/CodeWars-2.js
@@ -593,3 +593,17 @@ function solution(nums){
     return nums.sort((a,b)=>a-b);
 }
 console.log(solution([1, 2, 10, 50, 5]));
+
+/*
+//Array.prototype.join()  Sentence Smash
+Write a function that takes an array of words and smashes them together into a sentence and returns the sentence.
+    You can ignore any need to sanitize words or add punctuation, but you should add spaces between
+each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+Example
+    ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+*/
+// Smash Words
+function smash (words) {
+   return words.join(' ')
+};
+console.log(smash(['hello', 'world', 'this', 'is', 'great']));
