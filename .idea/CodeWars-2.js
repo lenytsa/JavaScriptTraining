4d55d2eec6a552e2886289544dfd89230dd2a39b
yelenaportnaya <yportnaya@hotmail.com>
2020-10-28 22:02:14 -0700

    CodeWars: Write shortest function to calculate Average number of Array. reduce() https://www.codewars.com/kata/56c22cdbe0c0f7cae2001789/solutions/javascript

--- .idea/CodeWars-2.js
+++ .idea/CodeWars-2.js
@@ -842,3 +842,19 @@ function findAverage(nums) {
     return (nums.reduce((acc, curr) => acc + curr))/nums.length
 }
 console.log(findAverage([1, 3, 5, 7]));
+console.log('------------------------------------');
+
+/*
+Write shortest function to calculate Average number of Array
+Given an array of integers, calculate the Average of these numbers.
+
+    Main challenge is to write shortest and compact function for it.
+
+                                                                     For example: var a = [0, 1, 2];
+avg(a) // output should be 1
+Output of function will be also checked in tests, however most important is to write the shortest possible function
+(code length < 100). Input will always be valid.*/
+function avg(a){
+    return (a.reduce((acc,val)=>acc + val))/a.length;
+}
+console.log(avg([0, 1, 2, 3, 4]));
