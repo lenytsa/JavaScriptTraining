b9d2740004afe98a75c650cac72e46d00f2b4c99
yelenaportnaya <yportnaya@hotmail.com>
2020-10-29 12:48:44 -0700

    Максимальная площадь прямоугольника. map() https://app.pasv.us/course/5d1cbf67086fc30038bf1859/lesson/5d33db4f0ca4f00038111266

--- .idea/arrayFilterExamples.js
+++ .idea/arrayFilterExamples.js
@@ -115,3 +115,28 @@ function reverseStr(word){
 //let word= 'strange';
 
 console.log(reverseStr('123456'));
+console.log('------------------------------------')
+
+//Array.prototype.map()
+/*
+question
+Имеется последовательность размеров прямоугольников, заданных парами своих сторон [a, b]:
+
+[[a0, b0], [a1, b1], ..., [an, bn]]. Найдите прямоугольник с максимальной площадью.
+
+    Напишите функцию maxRectangleSquare, которая принимает массив arr, содержащий размеры прямоугольников, в
+    качестве аргумента и возвращает максимальную площадь прямоугольника.
+
+    Пример:
+
+функция maxRectangleSquare([[3, 4], [8, 1], [2, 3]]) должна возвратить 12;
+функция maxRectangleSquare([[7, 7], [7, 8]]) должна возвратить 56;
+функция maxRectangleSquare([[2, 4]]) должна возвратить 8;
+функция maxRectangleSquare([]) должна возвратить null;*/
+function maxRectangleSquare(arr){
+    let sq = arr.map(el=>el.reduce((acc,cur)=>acc*cur,1));
+    console.log(sq)
+    return arr.length === 0? null : Math.max(...sq)
+}
+console.log(maxRectangleSquare([[3, 4], [8, 1], [2, 3]]));
+console.log(maxRectangleSquare([]));
