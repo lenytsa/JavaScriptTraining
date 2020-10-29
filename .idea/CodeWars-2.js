e1977652f7fe2d30b540cc5f4c768e0e5ea8665d
yelenaportnaya <yportnaya@hotmail.com>
2020-10-29 00:52:13 -0700

    CodeWars: Array plus array. reduce() https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/solutions/javascript

--- .idea/CodeWars-2.js
+++ .idea/CodeWars-2.js
@@ -917,3 +917,14 @@ function grow(x){
     return x.reduce((acc,val)=> acc*val);
 }
 console.log(grow([1, 2, 3, 4]));
+console.log('------------------------------------');
+
+/*Array plus array
+I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. ' +
+'I'll appreciate for your help.
+
+    P.S. Each array includes only integer numbers. Output is a number too.*/
+function arrayPlusArray(arr1, arr2) {
+    return arr1.reduce((a,b)=>a+b) +arr2.reduce((a,b)=>a+b);
+}
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]));
