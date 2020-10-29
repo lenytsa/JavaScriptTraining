39babc73f904c83363c92f241a70e0cefce52903
yelenaportnaya <yportnaya@hotmail.com>
2020-10-29 11:03:36 -0700

    CodeWars: Predict your age!. reduce() https://www.codewars.com/kata/5aff237c578a14752d0035ae/solutions/javascript

--- .idea/CodeWars-2.js
+++ .idea/CodeWars-2.js
@@ -928,3 +928,28 @@ function arrayPlusArray(arr1, arr2) {
     return arr1.reduce((a,b)=>a+b) +arr2.reduce((a,b)=>a+b);
 }
 console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]));
+console.log('------------------------------------');
+
+/*
+Predict your age!
+    My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
+
+    In honor of my grandfather's memory we will write a function using his formula!
+
+Take a list of ages when each of your great-grandparent died.
+    Multiply each number by itself.
+    Add them all together.
+    Take the square root of the result.
+    Divide by two.
+    Example
+predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
+Note: the result should be rounded down to the nearest integer.
+
+    Some random tests might fail due to a bug in the JavaScript implementation. Simply resubmit if that happens to you.*/
+
+function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
+    let ages = [age1,age2,age3,age4,age5,age6,age7,age8];
+    let res = ages.map((el) => el * el).reduce((sum, current) => sum + current, 0);
+    return Math.floor(Math.sqrt(res) / 2);
+}
+console.log(predictAge(65,60,75,55,60,63,64,45));
