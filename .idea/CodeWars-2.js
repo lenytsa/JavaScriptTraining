2dd1e56f79df0e09968f05e1a69e7bacaaa1e5bd
yelenaportnaya <yportnaya@hotmail.com>
2020-10-29 20:35:41 -0700

    Codewars:Training JS #15: Methods of Number object--toFixed(), toExponential() and toPrecision() https://www.codewars.com/kata/57256064856584bc47000611/solutions/javascript

--- .idea/CodeWars-2.js
+++ .idea/CodeWars-2.js
@@ -995,3 +995,26 @@ function toFreud(string) {
 }
 console.log(toFreud("This is a test"));//"sex sex sex sex"
 console.log(toFreud(""));
+console.log('------------------------------------');
+
+/*
+Training JS #15: Methods of Number object--toFixed(), toExponential() and toPrecision()
+Task
+Coding in function howManySmaller, function accept 2 parameter:arr and n. arr is a decimal array. n is a decimal.
+
+    The first mission: let all elements in the array keep two decimal places(No need to convert number n).
+
+The second mission: Traversal arr, count the number of the element which smaller than n and return it.
+
+    for example:
+
+    howManySmaller([1.234,1.235,1.228],1.24) should return 2
+howManySmaller([1.1888,1.1868,1.1838],1.19) should return 1
+howManySmaller([3.1288,3.1212,3.1205],3.1212) should return 2*/
+
+function howManySmaller(arr,n){
+    console.log(arr.filter(el=> el.toFixed(2)));
+    console.log(arr.map(el=> el.toFixed(2))); //note it returs a string
+   return  arr.filter(el=> el.toFixed(2)<n).length;
+}
+console.log(howManySmaller([1.234,1.235,1.228],1.24));
