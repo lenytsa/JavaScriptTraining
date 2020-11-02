8f41a79e3f4ed63bc80033ecb65c6647d368954d
yelenaportnaya <yportnaya@hotmail.com>
2020-11-01 22:44:22 -0800

    Codwars:Training JS #16: Methods of String object--slice(), substring() and substr() https://www.codewars.com/kata/57274562c8dcebe77e001012/solutions/javascript

--- .idea/CodeWars-2.js
+++ .idea/CodeWars-2.js
@@ -1129,3 +1129,34 @@ function array(arr){
 }
 console.log(array('1,2,3,4')); //2 3
 console.log(array('')); null
+
+console.log('----------------------');
+
+/*
+Training JS #16: Methods of String object--slice(), substring() and substr()
+Task
+Coding in function cutIt, function accept 1 parameter:arr. arr is a string array.
+
+    The first mission: Traversing arr, find the shortest string length.
+
+    The second mission: Traversing arr again, intercept all strings to the shortest string length(Start from index0).
+you can use one of slice() substring() or substr() do it. return the result after finished the work.
+
+    for example:
+    cutIt(["ab","cde","fgh"]) should return ["ab","cd","fg"]
+cutIt(["abc","defgh","ijklmn"]) should return ["abc","def","ijk"]
+cutIt(["codewars","javascript","java"]) should return ["code","java","java"]*/
+function cutIt(arr){
+   let arrLength=[];
+    for(let i=0; i<arr.length; i++){
+        arrLength.push(arr[i].length);
+    }
+    return arr.map(el=>el.slice(0,Math.min(...arrLength)));
+}
+console.log(cutIt(["ab","cde","fgh"]));
+//OR
+const cutIt1 = arr => {
+  const minLength = Math.min(...arr.map(str => str.length))
+  return arr.map(str => str.slice(0, minLength))
+}
+console.log(cutIt1(["ab","cde","fgh"]));
