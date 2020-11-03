dfed160ee7bc5188c56e0b2cf4fd65a24480c239
yelenaportnaya <yportnaya@hotmail.com>
2020-11-03 00:27:28 -0800

    Codwars:Grasshopper - Debug sayHello https://www.codewars.com/kata/5625618b1fe21ab49f00001f/solutions/javascript

--- .idea/CodeWars-string.js
+++ .idea/CodeWars-string.js
@@ -77,6 +77,20 @@ console.log("---------------------------");
 /*Grasshopper - Variable Assignment Debug
 Variable assignment
 Fix the variables assigments so that this code stores the string 'devLab' in the variable name.*/
-var a = "dev"
-var b = "Lab"
-var name = a + b
+var a = "dev";
+var b = "Lab";
+var name = a + b;
+console.log("---------------------------");
+
+/*
+Grasshopper - Debug sayHello
+Debugging sayHello function
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard.
+    It is your job to fix the code and get the program working again!
+
+    Example output:
+
+    Hello, Mr. Spock*/
+function sayHello(name) {
+    return `Hello, ${name}`;
+}
