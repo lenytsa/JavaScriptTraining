48dfe46e3f05146b4c1bc6ef304707516909b6a7
yelenaportnaya <yportnaya@hotmail.com>
2020-11-03 00:14:57 -0800

    Codwars:Template Strings https://www.codewars.com/kata/55a14f75ceda999ced000048/solutions/javascript

--- .idea/CodeWars-string.js
+++ .idea/CodeWars-string.js
@@ -28,3 +28,18 @@ Can you help her?*/
 function greet1(name){
     return name === "Johnny" ? "Hello, my love!" : "Hello, " + name + "!";
 }
+console.log("---------------------------");
+/*
+Template Strings
+Template Strings
+Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+Task
+Your task is to return the correct string using the Template String Feature.
+    Input
+Two Strings, no validation is needed.
+    Output
+You must output a string containing the two strings with the word ```' are '```
+Reference: https://developer.mozilla.org/en/docs/Web/Java*/
+var TempleStrings = function(obj, feature) {
+    return `${obj} are ${feature}`;
+}
