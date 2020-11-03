51f15a66da9d5b7f2611b6603e646e98627df8e2
yelenaportnaya <yportnaya@hotmail.com>
2020-11-03 00:17:51 -0800

    Codwars:Returning Strings https://www.codewars.com/kata/55a70521798b14d4750000a4/solutions/javascript

--- .idea/CodeWars-string.js
+++ .idea/CodeWars-string.js
@@ -43,3 +43,17 @@ Reference: https://developer.mozilla.org/en/docs/Web/Java*/
 var TempleStrings = function(obj, feature) {
     return `${obj} are ${feature}`;
 }
+console.log("---------------------------");
+
+/*
+Returning Strings
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello," +
+" <name> how are you doing today?".
+
+    SQL: return results in a column named greeting
+
+    [Make sure you type the exact thing I wrote or the program may not execute properly]
+*/
+function greet(name){
+    return `Hello, ${name} how are you doing today?`;
+}
