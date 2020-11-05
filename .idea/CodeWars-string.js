efc0bd30b7e4e53500db8c57994a9ad296a94a56
yelenaportnaya <yportnaya@hotmail.com>
2020-11-05 15:23:21 -0800

    Codwars:ES6 string addition https://www.codewars.com/kata/582e4c3406e37fcc770001ad/solutions/javascript

--- .idea/CodeWars-string.js
+++ .idea/CodeWars-string.js
@@ -157,6 +157,22 @@ function uefaEuro2016(teams, scores){
     if(scores[0]===scores[1]) return `At match ${teams[0]} - ${teams[1]}, teams played draw.`;
 }
 console.log(uefaEuro2016(['Germany', 'Ukraine'], [2, 0]));
+console.log("---------------------------");
 
+/*ES6 string addition
+It is easy to join two strings together like this string1 + string2.
 
+    Another way to get the desired result would be with string1.concat(string2)
 
+    ES6 has introduced another way of joining strings. Your task is to find out what this is and write a
+function that will add two strings together, there must be a space between the two strings.
+
++ , .concat() & .join() will not be allowed in this exercise. I have also removed some other methods that can
+be used to cheat!
+
+    If one of the arguments is a number your code must coerce it into being a string.*/
+
+function joinStrings(string1, string2){
+   return `${string1} ${string2}`;
+}
+console.log(joinStrings('string1', 'string2'));
