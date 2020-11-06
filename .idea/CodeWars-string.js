d3810358f339d0fd8769e74d609b8444b5e1ac87
yelenaportnaya <yportnaya@hotmail.com>
2020-11-05 19:53:47 -0800

    CodeWars:Is this my tail? https://www.codewars.com/kata/56f695399400f5d9ef000af5/solutions/javascript

--- .idea/CodeWars-string.js
+++ .idea/CodeWars-string.js
@@ -301,3 +301,30 @@ function getChar(c){
     return String.fromCodePoint(c);
 }
 console.log(getChar(65)); //A
+console.log("---------------------------");
+
+/*Is this my tail?
+    Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not
+    have the right tails. To help her, you must correct the broken function to make sure that the second argument
+(tail), is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
+
+If the tail is right return true, else return false.
+
+    The arguments will always be strings, and normal letters.
+
+    For Haskell, body has the type of String and tail has the type of Char. For Go, body has type string and tail
+has type rune.*/
+
+function correctTail(body, tail) {
+    sub = body.substr(body.length-tail.length);
+
+    if( sub === tail) {;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(correctTail("Rhino", "o"));
+console.log(correctTail("Meerkat", "t"));
+console.log(correctTail("Sgvotagjsgtnqngdqerdfowmenfyfpbul", "s"))
