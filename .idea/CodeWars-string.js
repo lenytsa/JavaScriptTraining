0decf19f4e0d719db81b01c83a7ac78ed89b97a9
yelenaportnaya <yportnaya@hotmail.com>
2020-11-05 19:34:02 -0800

    CodeWars:get character from ASCII Value https://www.codewars.com/kata/55ad04714f0b468e8200001c/solutions/javascript

--- .idea/CodeWars-string.js
+++ .idea/CodeWars-string.js
@@ -287,3 +287,17 @@ function insertSpaces1(str){
     return str.split('').join(' ');
 }
 console.log(insertSpaces1("abcde"));
+
+console.log("---------------------------");
+
+/*
+get character from ASCII Value
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+    Example:
+
+get_char(65) # => 'A'*/
+function getChar(c){
+    return String.fromCodePoint(c);
+}
+console.log(getChar(65)); //A
