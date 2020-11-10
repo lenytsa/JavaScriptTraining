4a5122a86b975aea362c16ad2fa406265d808260
yelenaportnaya <yportnaya@hotmail.com>
2020-11-09 18:40:49 -0800

    CodeWars:Remove String Spaces https://www.codewars.com/kata/57eae20f5500ad98e50002c5/solutions/javascript

--- .idea/CodeWars-string.js
+++ .idea/CodeWars-string.js
@@ -566,4 +566,11 @@ function tripleTrouble(one, two, three) {
     return s;
 }
 console.log(tripleTrouble("this","test","lock")); //ttlheoiscstk
+console.log('------------------');
 
+/*Remove String Spaces
+Simple, remove the spaces from the string, then return the resultant string.*/
+function noSpace(x) {
+    return x.replace(/[ ]/g, '');
+}
+console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
