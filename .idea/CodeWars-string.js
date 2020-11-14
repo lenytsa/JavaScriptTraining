65bb316164a4ed1be284b1efd4d373a6c6e20356
yelenaportnaya <yportnaya@hotmail.com>
2020-11-13 17:51:50 -0800

    CodeWars:Exes and Ohs https://www.codewars.com/kata/55908aad6620c066bc00002a

--- .idea/CodeWars-string.js
+++ .idea/CodeWars-string.js
@@ -821,4 +821,44 @@ function pattern1(n){
     }
     return output;
 }
-console.log(pattern1(6))
+console.log(pattern1(6));
+console.log('------------------');
+
+/*
+Exes and Ohs
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be
+case insensitive. The string can contain any char.
+
+    Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false*/
+function XO(str) {
+    let count_o = 0;
+    let count_x = 0;
+    str= str.toLowerCase();
+    for(let elm of str){
+        if(elm==='o') count_o ++;
+        if(elm === 'x') count_x ++
+    }
+    return count_o===count_x? true: false;
+}
+console.log(XO("xxOo"));
+//Other student solution
+function XO1(str) {
+    let x = str.match(/x/gi);
+    console.log(x);
+    let o = str.match(/o/gi);
+    console.log(o)
+    return (x && x.length) === (o && o.length);
+}
+console.log(XO1("xxOo"));
+//One more solution
+const XO2 = str => {
+    str = str.toLowerCase().split('');
+    return str.filter(x => x === 'x').length === str.filter(x => x === 'o').length;
+}
+console.log(XO2("xxOo"));
