8dba8098a63ea23c0524b053ead36874eafdb856
yelenaportnaya <yportnaya@hotmail.com>
2020-11-17 01:14:53 -0800

    CodeWars:Coding 3min : Jumping Dutch act https://www.codewars.com/kata/570bcd9715944a2c8e000009

--- .idea/CodeWars-string.js
+++ .idea/CodeWars-string.js
@@ -1335,3 +1335,52 @@ console.log(stringy(7));
 const stringy1 = size => "10".repeat(size).slice(0,size);
 // other interesting solutions #3
 const stringy2 = x => ''.padStart(x,'10');
+console.log('-------------------------');
+
+/*
+Coding 3min : Jumping Dutch act
+Coding 3min : Jumping Dutch act
+
+This is the simple version of Shortest Code series. If you need some challenges, please try the challenge version
+
+Task:
+    Mr. despair wants to jump off Dutch act, So he came to the top of a building.
+
+    Scientific research shows that a man jumped from the top of the roof, when the floor more than 6, the person will often die in an instant; When the floor is less than or equal to 6, the person will not immediately die, he would scream. (without proof)
+
+Input: floor, The height of the building (floor)
+
+Output: a string, The voice of despair(When jumping Dutch act)
+
+Example:
+    sc(2) should return "Aa~ Pa! Aa!"
+
+It means:
+
+    Mr. despair jumped from the 2 floor, the voice is "Aa~"
+He fell on the ground, the voice is "Pa!"
+He did not die immediately, and the final voice was "Aa!"
+sc(6) should return "Aa~ Aa~ Aa~ Aa~ Aa~ Pa! Aa!"
+
+sc(7) should return "Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Pa!"
+
+sc(10) should return "Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Pa!"
+
+if floor<=1, Mr. despair is safe, return ""
+
+*/
+function sc(floor){
+    console.log(floor)
+    if(floor<=1) return "";
+    if(floor<=6) return 'Aa~ '.repeat(floor-1) +'Pa! Aa!';
+    if(floor>6) return 'Aa~ '.repeat(floor-1) +'Pa!';
+    if(floor<3) return 'Aa~ Pa! Aa!';
+}
+console.log(sc(7) );
+console.log(sc(2) );
+//marked as best practice
+function sc1(floor){
+    if(floor <= 1) return "";
+
+    return 'Aa~ '.repeat(floor-1) + 'Pa!' + (floor<=6 ? ' Aa!': '');
+}
