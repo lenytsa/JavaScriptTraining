8462e9c0fe9712018e99b694ad966dddf3234676
yelenaportnaya <yportnaya@hotmail.com>
2020-11-17 15:26:22 -0800

    CodeWars:Name on billboard https://www.codewars.com/kata/570e8ec4127ad143660001fd/solutions/javascript

--- .idea/CodeWars-string.js
+++ .idea/CodeWars-string.js
@@ -1384,3 +1384,22 @@ function sc1(floor){
 
     return 'Aa~ '.repeat(floor-1) + 'Pa!' + (floor<=6 ? ' Aa!': '');
 }
+console.log('-------------------------');
+
+/*
+Name on billboard
+You can print your name on a billboard ad. Find out how much it will cost you. Each letter has a default price of £30, but that can be different if you are given 2 parameters instead of 1.
+
+You can not use multiplier "*" operator.
+
+    If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 leters * 30 = 600 (Space counts as a letter).*/
+
+function billboard(name, price = 30){
+    let total = 0;
+    for (let i = 1; i <= name.length; i++) {
+        total += price
+    }
+    return total;
+}
+console.log(billboard("Jeong-Ho Aristotelis"));
+console.log(billboard("Hadufuns John"));
