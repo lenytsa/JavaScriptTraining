6d7d08932cd903c9d9b93661b1cd80e9b9cb23eb
yelenaportnaya <yportnaya@hotmail.com>
2020-11-17 00:46:49 -0800

    CodeWars:repeatIt https://www.codewars.com/kata/557af9418895e44de7000053/solutions/javascript

--- .idea/CodeWars-string.js
+++ .idea/CodeWars-string.js
@@ -1292,3 +1292,21 @@ function validPassword(password){
 }
 console.log(validPassword("Qwertyu")); //Qwertyu
 console.log(validPassword("Null")); //Null$$$
+console.log('-------------------------');
+
+/*
+repeatIt
+Create a function that takes a string and an integer (n).
+
+    The function should return a string that repeats the input string n number of times.
+
+    If anything other than a string is passed in you should return "Not a string"
+
+Example
+"Hi", 2 --> "HiHi"
+1234, 5 --> "Not a string"*/
+var repeatIt = function(str, n) {
+    if(typeof str !== 'string') return 'Not a string'
+    return str.repeat(n);
+}
+console.log(repeatIt("*",3))
