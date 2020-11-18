2d4bfee9f674f87906b1447ebde39d7414516c1d
yelenaportnaya <yportnaya@hotmail.com>
2020-11-17 23:07:44 -0800

    CodeWars:Don't give me five! -- includes() https://www.codewars.com/kata/5813d19765d81c592200001a/solutions/javascript

--- .idea/CodeWars-string.js
+++ .idea/CodeWars-string.js
@@ -1511,6 +1511,47 @@ return s;
 console.log(calculate("Panda has 48 apples and loses 4"));
 console.log(calculate("Jerry has 34 apples and gains 6"));
 //Or other solutions
-function calculate(string) {
+function calculate1(string) {
     return eval(string.match(/\d+|lose|gain/g).join("").replace("lose", "-").replace("gain", "+"))
 }
+console.log('------------------------');
+
+/*
+Don't give me five!
+In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+    Examples:
+*/
+
+/*
+1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
+The result may contain fives. ;-)
+The start number will always be smaller than the end number. Both numbers can be also negative!
+
+    I'm very curious for your solutions and the way you solve it. Maybe someone of you will find an easy pure mathematics solution.
+*/
+
+function dontGiveMeFive(start, end) {
+    let count = 0;
+    for (let i = start; i <= end; i++) {
+        if (i.toString().includes(5) === false) {
+            count++
+        }
+    }
+    return count
+}
+console.log(dontGiveMeFive(1,9));
+//Other solutions #2
+function dontGiveMeFive1(start, end)
+{
+    var count = 0;
+    for(; start <= end; start++) {
+        if(start.toString().indexOf("5") == -1) {
+            count++;
+        }
+    }
+    return count;
+}
+//Other solutions #3
+const dontGiveMeFive2=(s,e)=>[...Array(e-s+1)].reduce((r,_,n)=>r+!/5/.test(n+s),0)
