e178e4744f8ed470822ee4e3bd4a755b1e3a50a1
yelenaportnaya <yportnaya@hotmail.com>
2020-11-19 01:08:12 -0800

    CodeWars: Create Phone Number      slice() https://www.codewars.com/kata/525f50e3b73515a6db000b83/solutions/javascript

--- .idea/CodeWars-string.js
+++ .idea/CodeWars-string.js
@@ -1932,4 +1932,33 @@ console.log(automorphic(1));
 console.log(automorphic(3));
 console.log(automorphic(6));
 
+console.log('-----------slice() ---------------------')
+/*
+
+Create Phone Number
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers
+in the form of a phone number.
+    Example:createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+    Don't forget the space after the closing parentheses!
+*/
+function createPhoneNumber(numbers){
+    let area= numbers.slice(0,3).join('');
+    let midd = numbers.slice(3,6).join('');
+    let last = numbers.slice(-4).join('');
+    return `(${area}) ${midd}-${last}`;
+}
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+//Another nice solution
+function createPhoneNumber1(numbers){
+    var format = "(xxx) xxx-xxxx";
+    for(var i = 0; i < numbers.length; i++)
+    {
+        format = format.replace('x', numbers[i]);
+    }
+    return format;
+}
+console.log(createPhoneNumber1([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+
 
