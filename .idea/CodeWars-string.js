c3021a63bb92ba35a2e4afdb7abe0c2853bd3264
yelenaportnaya <yportnaya@hotmail.com>
2020-11-18 22:37:02 -0800

    CodeWars: String ends with? https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/solutions/javascript

--- .idea/CodeWars-string.js
+++ .idea/CodeWars-string.js
@@ -1903,3 +1903,15 @@ function validateCode1(code)
 {
     return /^[123]/.test(code);
 }
+console.log('-----------endsWith()---------------------')
+/*
+String ends with?
+    Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+    solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false*/
+function solution(str, ending){
+    return str.endsWith(ending);
+}
