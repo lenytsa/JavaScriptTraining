45ddb44056274cc66e79fb765ebb13aaa7618767
yelenaportnaya <yportnaya@hotmail.com>
2020-11-18 17:32:27 -0800

    CodeWars: Find the position!     IndexOf() examples https://www.codewars.com/kata/568dc69683322417eb00002c/solutions/javascript

--- .idea/CodeWars-string.js
+++ .idea/CodeWars-string.js
@@ -1763,3 +1763,16 @@ const tripleX2 = str => {
     let x = str.indexOf('x')
     return x > -1 && x === str.indexOf('xxx')
 }
+console.log('--------------IndexOf---------------------------');
+
+/*
+Find the position!
+    When provided with a letter, return its position in the alphabet.
+Input :: "a"
+Ouput :: "Position of alphabet: 1"
+This kata is meant for beginners. Rank and upvote to bring it out of beta*/
+function position(letter){
+    let alphabet ='abcdefghijklmnopqrstuvwxyz';
+return `Position of alphabet: ${alphabet.indexOf(letter)+1}`;
+}
+console.log(position("z"))
