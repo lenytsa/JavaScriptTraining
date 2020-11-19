528339ae20a1551aa1368de19a3e1d52d87ec5e9
yelenaportnaya <yportnaya@hotmail.com>
2020-11-18 22:21:03 -0800

    CodeWars: validate code with simple regex https://www.codewars.com/kata/56a25ba95df27b7743000016/solutions/javascript

--- .idea/CodeWars-string.js
+++ .idea/CodeWars-string.js
@@ -1885,3 +1885,21 @@ console.log(firstToLast("ababc","d"));
 function firstToLast1(str,c){
     return (str.indexOf(c) < 0) ? -1 : str.lastIndexOf(c) - str.indexOf(c);
 }
+console.log('-------------startsWith()----------------------------');
+
+/*validate code with simple regex
+Basic regex tasks. Write a function that takes in a numeric code of any length. The function should check if the
+    code begins with 1, 2, or 3 and return true if so. Return false otherwise.
+
+    You can assume the input will always be a number.*/
+function validateCode (code) {
+    code= code.toString()
+    return code.startsWith([1])|| code.startsWith([2])|| code.startsWith([3])
+}
+console.log(validateCode(123));
+console.log(validateCode(248));
+//short solution
+function validateCode1(code)
+{
+    return /^[123]/.test(code);
+}
