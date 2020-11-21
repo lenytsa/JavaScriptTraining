a48f60fb73d4df443fa7c41a412a151111c360d4
yelenaportnaya <yportnaya@hotmail.com>
2020-11-21 00:32:15 -0800

    CodeWars:Complementary DNA https://www.codewars.com/kata/554e4a2f232cdd87d9000038/solutions/javascript

--- .idea/CodeWars-string.js
+++ .idea/CodeWars-string.js
@@ -2173,3 +2173,45 @@ function shortcut1(string){
     return string.replace(/[aeiou]/g,'')
 }
 console.log(shortcut1("codewars"));
+console.log('-----------replace() ---------------------');
+
+/*
+Complementary DNA
+Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for
+    the development and functioning of living organisms.
+
+    If you want to know more http://en.wikipedia.org/wiki/DNA
+
+    In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have function with one side
+of the DNA (string, except for Haskell); you need to get the other complementary side. DNA strand is never empty or
+there is no DNA at all (again, except for Haskell).
+
+    More similar exercise are found here http://rosalind.info/problems/list-view/ (source)
+    DNAStrand ("ATTGC") // return "TAACG"
+
+DNAStrand ("GTAT") // return "CATA" */
+function DNAStrand(dna) {
+    dna.split('');
+    let s=''
+    for(let i=0; i<dna.length; i++){
+        if(dna[i]==='A') s= s+ 'T';
+        if(dna[i]==='T') s= s+ 'A';
+        if(dna[i]==='G') s= s+ 'C';
+        if(dna[i]==='C') s= s+ 'G';
+    }
+    return s;
+}
+console.log(DNAStrand ("GTAT")); //CATA
+
+//Others solutions
+function DNAStrand1(dna){
+    return dna
+        .replace(/A/g, 't')
+        .replace(/T/g, 'a')
+        .replace(/C/g, 'g')
+        .replace(/G/g, 'c').toUpperCase();
+}
+console.log(DNAStrand1("GTAT")); //CATA
+//other user solution
+let pairs = { A: "T", T: "A", C: "G", G: "C" };
+const DNAStrand4 = dna => dna.replace(/./g, c => pairs[c]);
