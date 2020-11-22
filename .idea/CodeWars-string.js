31881722c1bf0fbc7ec9b643be70867000867140
yelenaportnaya <yportnaya@hotmail.com>
2020-11-21 23:58:57 -0800

    CodeWars:FIXME: Replace all dots --replace() https://www.codewars.com/kata/596c6eb85b0f515834000049/solutions/javascript

--- .idea/CodeWars-string.js
+++ .idea/CodeWars-string.js
@@ -2454,3 +2454,20 @@ Examples:
 function removeO(string){
 return string.replace(/[o]/g,'');
 }
+console.log('-----------replace() ---------------------');
+/*
+
+FIXME: Replace all dots
+The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+But it's not working properly.
+
+Task
+Fix the bug so we can all go home early.
+
+    Notes
+String str will never be null.*/
+var replaceDots = function(str) {
+    return str.replace(/[.]/g, '-');
+}
+console.log(replaceDots("one.two.three"));
