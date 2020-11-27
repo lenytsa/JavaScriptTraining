9932afb3baa545d043b306a4b382016f53e27d6b
yelenaportnaya <yportnaya@hotmail.com>
2020-11-27 00:09:18 -0800

    Codewars:Anagram Detection --split() https://www.codewars.com/kata/529eef7a9194e0cbc1000255

--- .idea/splitExamples.js
+++ .idea/splitExamples.js
@@ -161,3 +161,45 @@ var palindromeChainLength = function(n) {
     return counter;
 };
 console.log(palindromeChainLength(82));
+//Others codewars solutions
+var palindromeChainLength  = function(n) {
+    var x = parseInt( (""+n).split('').reverse().join('') );
+    if(n != x){
+        return 1 + palindromeChainLength (n + x);
+    }
+    return 0;
+};
+//Others codewars solution
+function palindromeChainLength(n) {
+    var count = 0, rev = 0;
+    while(n != (rev = parseInt(n.toString().split('').reverse().join('')))) {
+        n += rev;
+        count++;
+    }
+    return count;
+};
+
+console.log('------------------------');
+/*
+Anagram Detection
+An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+Note: anagrams are case insensitive
+
+Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+    Examples
+"foefet" is an anagram of "toffee"
+
+"Buckethead" is an anagram of "DeathCubeK"*/
+
+// write the function isAnagram
+let isAnagram = function (str1, str2){
+    return str1.toLowerCase().split('').sort().join('') === str2.toLowerCase().split('').sort().join('')
+
+}
+console.log(isAnagram("foefet", "toffee")); //true
+console.log(isAnagram("dumble", "bumble")); //false
+console.log('------------------------');
+
+
