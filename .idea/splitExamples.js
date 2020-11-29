5aba728fa671a6fb4e4a6b63facd3e8597fe1f5e
yelenaportnaya <yportnaya@hotmail.com>
2020-11-29 15:09:10 -0800

    Codewars:Name Shuffler --split() https://www.codewars.com/kata/559ac78160f0be07c200005a/solutions/javascript

--- .idea/splitExamples.js
+++ .idea/splitExamples.js
@@ -251,3 +251,22 @@ function highAndLow(numbers){
     numbers = numbers.split(' ').map(Number);
     return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
 }
+console.log('------------------------');
+
+/*Name Shuffler
+Write a function that returns a string in which firstname is swapped with last name.
+
+nameShuffler('john McClane'); => "McClane john"*/
+function nameShuffler(str){
+    str = str.split(' ');
+    console.log(str);
+    return str[1] + ' ' + str[0];
+}
+console.log(nameShuffler('john McClane'));
+//Or other solutions from codewars
+function nameSuffle(str){
+    return str.split(' ').reverse().join(' ')
+}
+//Or other solutions from codewars
+const nameShuffler = str =>
+    str.replace(/([^\s]+) ([^\s]+)/, `$2 $1`);
