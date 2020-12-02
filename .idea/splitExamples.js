e6688ac93632dfdc4c6fc0a4ef780a1cd62a2d0d
yelenaportnaya <yportnaya@hotmail.com>
2020-12-02 00:29:56 -0800

    Codewars:Regex validate PIN code --split() https://www.codewars.com/kata/55f8a9c06c018a0d6e000132/solutions/javascript

--- .idea/splitExamples.js
+++ .idea/splitExamples.js
@@ -297,3 +297,34 @@ function addLength1(str){
 }
 //Or
 var addLength2 = s => s.split(' ').map(x => x + ' ' + x.length);
+
+console.log('------------------------');
+
+/*
+Regex validate PIN code
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+    If the function is passed a valid PIN string, return true, else return false.
+"1234"   -->  true
+"12345"  -->  false
+"a234"   -->  false*/
+function validatePIN (pin) {
+    return /^(\d{4}|\d{6})$/.test(pin);
+}
+console.log(validatePIN("234"));//false
+//Other codewars solutions
+function validatePIN1 (pin) {
+    var reg = new RegExp('^([0-9]{4}|[0-9]{6})$');
+    return reg.test(pin);
+}
+//Other codewars solutions
+function validatePIN (pin) {
+    //return true or false
+    var n = pin.length;
+    if( n != 4 && n != 6)
+        return false;
+    for (var i in pin)
+        if (pin[i] > '9' || pin[i] < '0')
+            return false;
+    return true;
+}
