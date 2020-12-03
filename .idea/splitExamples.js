a02d82be3525a3d7cffd7502bfa1bf8841b6af52
yelenaportnaya <yportnaya@hotmail.com>
2020-12-02 16:38:13 -0800

    Codewars:Reversing a string --split() https://www.codewars.com/kata/562e8a47ded765e36a00006b/solutions/javascript

--- .idea/splitExamples.js
+++ .idea/splitExamples.js
@@ -328,3 +328,19 @@ function validatePIN (pin) {
             return false;
     return true;
 }
+console.log('------------------------');
+
+/*
+Reversing a string
+Create a function that will reverse a string.
+
+    You might have to convert the string into an array first before you are able to reverse it.
+
+    Your final answer must also be a string.
+
+    Some functions you might find useful to solve this problem are: String.split() Array.reverse() Array.join()*/
+function reverseString(str) {
+return str.split('').reverse().join('');
+
+}
+console.log(reverseString("nice"))
