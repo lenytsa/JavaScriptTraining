6209f0cfc7c3bf7e6076d869b9d3c741d5c752dd
yelenaportnaya <yportnaya@hotmail.com>
2020-12-03 01:11:10 -0800

    Codewars:Convert a string to an array --split() https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/solutions/javascript

--- .idea/splitExamples-2.js
+++ .idea/splitExamples-2.js
@@ -127,3 +127,16 @@ function ones(n) {
 function ones(n) {
     return n.toString(2).split('').reduce((sum, x) => sum + +x, 0)
 }
+
+/*
+Convert a string to an array
+Write a function to split a string and convert it into an array of words. For example:
+
+    "Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]*/
+function stringToArray(string){
+    return string.split(' ');
+
+}
+console.log(stringToArray("Robin Singh"));
