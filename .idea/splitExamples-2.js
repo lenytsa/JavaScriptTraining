ea71128dc9b991585747766849e8db40832a15ad
yelenaportnaya <yportnaya@hotmail.com>
2020-12-04 15:37:37 -0800

    Codewars:Exclamation marks series #13: Count the number of exclamation marks and question marks, return the product --split() https://www.codewars.com/kata/57fb142297e0860073000064/solutions/javascript

--- .idea/splitExamples-2.js
+++ .idea/splitExamples-2.js
@@ -160,3 +160,38 @@ function reverser(sentence) {
     return sentence.split('').reverse().join('').split(' ').reverse().join(' ')
 }
 console.log(reverser("Hello mother")); //olleH rehtom
+
+/*
+Exclamation marks series #13: Count the number of exclamation marks and question marks, return the product
+Description:
+    Count the number of exclamation marks and question marks, return the product.
+
+    Examples
+Product("") == 0
+product("!") == 0
+Product("!ab? ?") == 2
+Product("!!") == 0
+Product("!??") == 2
+Product("!???") == 3
+Product("!!!??") == 6
+Product("!!!???") == 9
+Product("!???!!") == 9
+Product("!????!!!?") == 20*/
+function product(s) {
+    s = s.split('');
+    console.log(s)
+    let questonMark = s.filter(el => el === '?').length; //3
+    let exclamationMark = s.filter(el => el === '!').length; //3
+    return questonMark * exclamationMark;
+    console.log(questonMark, exclamationMark);//9
+}
+console.log(product("!!!???"));
+//other codewars solutions
+function product2(s){
+    excs = s.split('!').length - 1
+    ques = s.split('?').length - 1
+    return excs * ques
+}
+//Other codewars solutions
+const product3 = s =>
+    s.replace(/[^!]/g, ``).length * s.replace(/[^?]/g, ``).length;
