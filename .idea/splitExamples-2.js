42dfb2dbb394f501db1d8a9ac5d10279f0cd8533
yelenaportnaya <yportnaya@hotmail.com>
2020-12-04 15:29:25 -0800

    Codewars:Reverse Letters in Sentence --split() https://www.codewars.com/kata/57ebdf944cde58f973000405/solutions/javascript

--- .idea/splitExamples-2.js
+++ .idea/splitExamples-2.js
@@ -137,6 +137,26 @@ Write a function to split a string and convert it into an array of words. For ex
 "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]*/
 function stringToArray(string){
     return string.split(' ');
-
 }
 console.log(stringToArray("Robin Singh"));
+console.log('----------------------');
+
+/*
+Reverse Letters in Sentence
+Take a sentence (string) and reverse each word in the sentence. Do not reverse the order of the words, just the letters in each word.
+
+    If there is punctuation, it should be interpreted as a regular character; no special rules.
+
+    If there is spacing before/after the input string, leave them there.
+
+    String will not be empty.
+
+    Examples
+"Hi mom" => "iH mom"
+" A fun little challenge! " => " A nuf elttil !egnellahc "*/
+// reverse the letters in the sentence
+
+function reverser(sentence) {
+    return sentence.split('').reverse().join('').split(' ').reverse().join(' ')
+}
+console.log(reverser("Hello mother")); //olleH rehtom
