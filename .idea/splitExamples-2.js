8cbe6fd1a7d56e786259d3cc8ff7dd6f6257717d
yelenaportnaya <yportnaya@hotmail.com>
2020-12-09 21:25:59 -0800

    Codewars:Responsible Drinking --split() https://www.codewars.com/kata/5aee86c5783bb432cd000018/solutions/javascript

--- .idea/splitExamples-2.js
+++ .idea/splitExamples-2.js
@@ -373,3 +373,44 @@ function solve1(eq){
   return eq.split(/([*\+\-\/])/).reverse().join('');
 }
 console.log('----------------------------');
+
+
+/*
+Responsible Drinking
+Welcome to the Codewars Bar!
+    Codewars Bar recommends you drink 1 glass of water per standard drink so you're not hungover tomorrow morning.
+
+Your fellow coders have bought you several drinks tonight in the form of a string. Return a string suggesting how many glasses of water you should drink to not be hungover.
+
+    Examples
+"1 beer"  -->  "1 glass of water"
+because you drank one standard drink
+"1 beer"  -->  "1 glass of water"
+because you drank one standard drink
+
+"1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"  -->  "10 glasses of water"
+because you drank ten standard drinks
+Note:
+
+    To keep the things simple, we'll consider that any "numbered thing" in the string is a drink. Even "1 bear" -> "1 glass of water"; or "1 chainsaw and 2 pools" -> "3 glasses of water"...
+
+*/
+function hydrate(s) {
+    let sum= s.replace(/\D/g, '').split('').map(el=>+el).reduce((a,b)=>a+b,0)
+    return (sum === 1)? "1 glass of water" : `${sum} glasses of water`;
+}
+console.log(hydrate("2 glasses of wine and 1 shot")); //3 glasses of water
+console.log(hydrate('1 beer 2 glasses of wine and 1 shot 1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer'));
+
+//Other codewars solutions
+function hydrate1(s) {
+    let answer = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (parseInt(s[i]) > 0) {
+            answer += +s[i];
+        }
+    }
+    return answer > 1 ? `${answer} glasses of water`: '1 glass of water'
+}
+//Other codewars
+const hydrate2 = (s, w = [...s].filter(x => !isNaN(x)).reduce((a, b) => a + +b, 0)) => `${w} glass${w === 1 ? '' : 'es'} of water`;
