6557adfc7bae6a09f3b05c4dd4ab4d841dd4ef87
yelenaportnaya <yportnaya@hotmail.com>
2020-12-17 15:48:23 -0800

    Codewars:What's wrong with these identifiers? https://www.codewars.com/kata/56bb01de0e8b29de50000b19/solutions/javascript/me/best_practice

--- .idea/splitExamples-2.js
+++ .idea/splitExamples-2.js
@@ -662,3 +662,19 @@ const rooms = {
     kitchenRoom : [4,5,6],
     bathRoom    : [7,8,9]
 }
+console.log('-----------------');
+
+/*
+What's wrong with these identifiers?
+Why does CodeWars not have the ability to DELETE old Katas?
+    I cannot delete this, I cannot change the tests to improve it as they are locked for some reason. Instread people still try and complete this kata and rightfully add feedback that it's furstrating and makes little sense.
+
+I can't understand why the functionality still has not been added to delete katas.
+
+PLEASE DO NOT ATTEMPT TO DO THIS KATA*/
+let Person = {
+    city: 'Miami',
+    state: 'FL',
+    country: 'USA',
+    street: 'Main'
+}
